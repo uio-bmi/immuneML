581d75ee24b7d612334986a688614ac817c7f81f
pavlovicmilena <milenapavlovic775@gmail.com>
2019-03-21 15:57:53 +0100

    add matching_sequence report

---
+++ source/reports/encoding_reports/MatchingSequenceDetails.py
@@ -0,0 +1,76 @@
+import csv
+
+from source.analysis.SequenceMatcher import SequenceMatcher
+from source.data_model.dataset.Dataset import Dataset
+from source.data_model.receptor_sequence.ReceptorSequence import ReceptorSequence
+from source.data_model.repertoire.Repertoire import Repertoire
+from source.reports.encoding_reports.EncodingReport import EncodingReport
+from source.util.PathBuilder import PathBuilder
+
+
+class MatchingSequenceDetails(EncodingReport):
+    """
+    TODO: write description here
+
+    params:
+        - list of reference sequences
+        - max Levenshtein distance
+    """
+
+    def generate(self, dataset: Dataset, result_path: str, params: dict):
+
+        PathBuilder.build(result_path)
+        self._make_overview(dataset, result_path, params)
+        self._make_matching_report(dataset, result_path, params)
+
+    def _make_overview(self, dataset: Dataset, result_path: str, params: dict):
+        filename = result_path + "matching_sequence_overview.tsv"
+        with open(filename, "w") as file:
+            csv_writer = csv.DictWriter(file, fieldnames=["patient", "chain", "matching_sequence_count",
+                                                          "repertoire_size", "max_levenshtein_distance"], delimiter="\t")
+            csv_writer.writeheader()
+            for index, repertoire in enumerate(dataset.get_data()):
+                csv_writer.writerow({
+                    "patient": repertoire.identifier,
+                    "chain": str(list({seq.metadata.chain for seq in repertoire.sequences}))[1:-1],
+                    "matching_sequence_count": dataset.encoded_data["repertoires"][index][0],
+                    "repertoire_size": len(repertoire.sequences),
+                    "max_levenshtein_distance": params["max_distance"]
+                })
+        return filename
+
+    def _make_matching_report(self, dataset: Dataset, result_path: str, params: dict):
+
+        filenames = []
+
+        for repertoire in dataset.get_data():
+            filenames.append(self._make_repertoire_report(repertoire, result_path, params))
+
+        return filenames
+
+    def _make_repertoire_report(self, repertoire: Repertoire, result_path: str, params: dict):
+
+        filename = result_path + "{}_{}.tsv".format(repertoire.identifier,
+                                                    list({seq.metadata.chain for seq in repertoire.sequences}))
+
+        with open(filename, "w") as file:
+            csv_writer = csv.DictWriter(file, fieldnames=["sequence", "v_gene", "j_gene", "chain", "matching_sequences", "max_distance"], delimiter="\t")
+            csv_writer.writeheader()
+            for index, sequence in enumerate(repertoire.sequences):
+
+                matching_sequences = self._find_matching_sequences(sequence, params["reference_sequences"], params["max_distance"])
+
+                csv_writer.writerow({
+                    "sequence": sequence.get_sequence(),
+                    "v_gene": sequence.metadata.v_gene,
+                    "j_gene": sequence.metadata.j_gene,
+                    "chain": sequence.metadata.chain,
+                    "matching_sequences": str(matching_sequences)[1:-1].replace("'", ""),
+                    "max_distance": params["max_distance"]
+                })
+
+        return filename
+
+    def _find_matching_sequences(self, sequence: ReceptorSequence, reference_sequences: list, max_distance: int):
+        matcher = SequenceMatcher()
+        return matcher.match_sequence(sequence, reference_sequences, max_distance)["matching_sequences"]
---
+++ test/reports/encoding_reports/test_matchingSequenceDetails.py
@@ -0,0 +1,46 @@
+import glob
+import os
+import shutil
+from unittest import TestCase
+
+import pandas as pd
+
+from source.data_model.dataset.Dataset import Dataset
+from source.data_model.receptor_sequence.ReceptorSequence import ReceptorSequence
+from source.data_model.receptor_sequence.SequenceMetadata import SequenceMetadata
+from source.environment.EnvironmentSettings import EnvironmentSettings
+from source.reports.encoding_reports.MatchingSequenceDetails import MatchingSequenceDetails
+from source.util.RepertoireBuilder import RepertoireBuilder
+
+
+class TestMatchingSequenceDetails(TestCase):
+    def test_generate(self):
+        path = EnvironmentSettings.root_path + "test/tmp/encrepmatchingseq/"
+        filenames = RepertoireBuilder.build([["AAA", "CCC"], ["AAC", "ASDA"], ["CCF", "ATC"]], path)
+        ref_seqs = [ReceptorSequence("AAA", metadata=SequenceMetadata()),
+                    ReceptorSequence("CCF", metadata=SequenceMetadata())]
+        dataset = Dataset(filenames=filenames,
+                          encoded_data={
+                              "repertoires": [[2], [1], [1]],
+                              "labels": [[1, 0, 0]],
+                              "label_names": ["default"],
+                              "feature_names": ["percentage_of_sequences_matched"]
+                          })
+
+        report = MatchingSequenceDetails()
+
+        report.generate_report({
+            "dataset": dataset,
+            "result_path": path + "result/",
+            "reference_sequences": ref_seqs,
+            "max_distance": 1
+        })
+
+        self.assertTrue(os.path.isfile(path + "result/matching_sequence_overview.tsv"))
+        self.assertEqual(4, len([name for name in glob.glob(path + "result/*.tsv") if os.path.isfile(name)]))
+
+        df = pd.read_csv(path + "result/matching_sequence_overview.tsv", sep="\t")
+        self.assertTrue(all([key in df.keys() for key in ["patient", "chain", "matching_sequence_count", "repertoire_size", "max_levenshtein_distance"]]))
+        self.assertEqual(3, df.shape[0])
+
+        shutil.rmtree(path)
