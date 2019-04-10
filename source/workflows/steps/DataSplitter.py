eef1da4f39f95d80ac1b663a6eaacceba188df71
pavlovicmilena <milenapavlovic775@gmail.com>
2019-04-10 15:13:35 +0200

    refactor steps to work with dsl parsers

--- source/workflows/steps/DataSplitter.py
+++ source/workflows/steps/DataSplitter.py
@@ -1,7 +1,8 @@
-# quality: gold
-
 import copy
 import random
+
+import numpy as np
+from sklearn.model_selection import KFold
 
 from source.data_model.dataset.Dataset import Dataset
 from source.workflows.steps.Step import Step
@@ -16,26 +17,60 @@ class DataSplitter(Step):
 
     @staticmethod
     def check_prerequisites(input_params: dict = None):
-        assert input_params is not None, "DataSplitter: input_params were not set. They need to include the dataset to split and the training percentage."
-        assert "dataset" in input_params and isinstance(input_params["dataset"], Dataset), "DataSplitter: the dataset has to be set in input_params and has to be an instance of Dataset class."
-        # assert "training_percentage" in input_params \
-        #       and isinstance(input_params["training_percentage"], float) \
-        #       and 0 <= input_params["training_percentage"] <= 1,
-        #       "DataSplitter: training_percentage parameter has to be set and it has to be a float between 0 and 1."
+        assert input_params is not None, \
+            "DataSplitter: input_params were not set. " \
+            "They need to include the dataset to split and the training percentage."
+        assert "dataset" in input_params and isinstance(input_params["dataset"], Dataset), \
+            "DataSplitter: the dataset has to be set in input_params and has to be an instance of Dataset class."
 
     @staticmethod
     def perform_step(input_params: dict = None):
+        fn = getattr(DataSplitter, "{}_split".format(input_params["assessment_type"].lower()))
+        return fn(input_params)
+
+    @staticmethod
+    def loocv_split(input_params: dict):
+        input_params["count"] = input_params["dataset"].get_repertoire_count()
+        return DataSplitter.k_fold_cv_split(input_params)
+
+    @staticmethod
+    def k_fold_cv_split(input_params: dict):
+        dataset = input_params["dataset"]
+        splits_count = input_params["count"]
+        train_datasets, test_datasets = [], []
+        random.shuffle(dataset.filenames)
+        dataset.filenames = np.array(dataset.filenames)
+
+        k_fold = KFold(n_splits=splits_count)
+        for train_index, test_index in k_fold.split(dataset.filenames):
+            train_dataset = copy.deepcopy(dataset)
+            train_dataset.filenames = train_dataset.filenames[train_index]
+            train_datasets.append(train_dataset)
+
+            test_dataset = copy.deepcopy(dataset)
+            test_dataset.filenames = test_dataset.filenames[test_index]
+            test_datasets.append(test_dataset)
+
+        return train_datasets, test_datasets
+
+    @staticmethod
+    def random_split(input_params: dict):
 
         dataset = input_params["dataset"]
         training_percentage = input_params["training_percentage"]
         train_count = int(len(dataset.filenames) * training_percentage)
+        train_datasets, test_datasets = [], []
 
-        random.shuffle(dataset.filenames)
+        for i in range(input_params["count"]):
 
-        train_dataset = copy.deepcopy(dataset)
-        train_dataset.filenames = train_dataset.filenames[:train_count]
+            random.shuffle(dataset.filenames)
 
-        test_dataset = copy.deepcopy(dataset)
-        test_dataset.filenames = test_dataset.filenames[train_count:]
+            train_dataset = copy.deepcopy(dataset)
+            train_dataset.filenames = train_dataset.filenames[:train_count]
+            train_datasets.append(train_dataset)
 
-        return train_dataset, test_dataset
+            test_dataset = copy.deepcopy(dataset)
+            test_dataset.filenames = test_dataset.filenames[train_count:]
+            test_datasets.append(test_dataset)
+
+        return train_datasets, test_datasets
