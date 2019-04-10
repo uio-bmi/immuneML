69819f79a2fb1af847b6e068f4a6ace4292e4243
pavlovicmilena <milenapavlovic775@gmail.com>
2019-04-10 15:11:08 +0200

    update gapped motifs not to include /

--- test/simulation/signal_implanting_strategy/test_healthySequenceImplanting.py
+++ test/simulation/signal_implanting_strategy/test_healthySequenceImplanting.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 
-from source.data_model.repertoire.Repertoire import Repertoire
 from source.data_model.receptor_sequence.ReceptorSequence import ReceptorSequence
+from source.data_model.repertoire.Repertoire import Repertoire
 from source.simulation.implants.Motif import Motif
 from source.simulation.implants.Signal import Signal
-from source.simulation.motif_instantiation_strategy.IdentityMotifInstantiation import IdentityMotifInstantiation
+from source.simulation.motif_instantiation_strategy.IdentityInstantiation import IdentityInstantiation
 from source.simulation.signal_implanting_strategy.HealthySequenceImplanting import HealthySequenceImplanting
 from source.simulation.signal_implanting_strategy.sequence_implanting.GappedMotifImplanting import GappedMotifImplanting
 
@@ -13,7 +13,7 @@ class TestHealthySequenceImplanting(TestCase):
     def test_implant_in_repertoire(self):
         repertoire = Repertoire([ReceptorSequence(amino_acid_sequence="ACDFQ"), ReceptorSequence(amino_acid_sequence="TGCDF")])
         implanting = HealthySequenceImplanting(GappedMotifImplanting())
-        signal = Signal(1, [Motif("m1", IdentityMotifInstantiation(), "CCC")], implanting)
+        signal = Signal(1, [Motif("m1", IdentityInstantiation(), "CCC")], implanting)
 
         repertoire2 = implanting.implant_in_repertoire(repertoire, 0.5, signal)
 
@@ -23,7 +23,7 @@ class TestHealthySequenceImplanting(TestCase):
 
     def test_implant_in_sequence(self):
         implanting = HealthySequenceImplanting(GappedMotifImplanting())
-        signal = Signal(1, [Motif("m1", IdentityMotifInstantiation(), "CCC")], implanting)
+        signal = Signal(1, [Motif("m1", IdentityInstantiation(), "CCC")], implanting)
         sequence = ReceptorSequence(amino_acid_sequence="ACDFQ")
         sequence2 = implanting.implant_in_sequence(sequence, signal)
 
