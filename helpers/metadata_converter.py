54ad720880be639b83d95dff222172d96b391690
pavlovicmilena <milenapavlovic775@gmail.com>
2019-05-03 13:33:07 +0200

    add arbitrary class names to metadata_converter

--- helpers/metadata_converter.py
+++ helpers/metadata_converter.py
@@ -5,21 +5,21 @@ import sys
 import pandas as pd
 
 
-def convert_metadata(path, result_path, extension: str = "csv"):
+def convert_metadata(path, result_path, disease_name: str, negative_class_name: str, extension: str = "csv"):
     files = [f for f in glob.glob(path + "**/*.{}".format(extension), recursive=True)]
 
     donors = []
     chains = []
-    celiacs = []
+    disease = []
     filenames = []
     for file in files:
         filename = os.path.basename(file).split(".")[0]
         donors.append(filename.split('_')[0])
-        chains.append(filename.split("_")[2][-1])
-        celiacs.append(True if "_CD" in file and "_HC" not in file else False)
+        chains.append(filename.split("_")[-1][-1])
+        disease.append(True if disease_name in file and negative_class_name not in file else False)
         filenames.append(file)
 
-    df = pd.DataFrame.from_dict({"donor": donors, "chain": chains, "celiac": celiacs, "filename": filenames})
+    df = pd.DataFrame.from_dict({"donor": donors, "chain": chains, disease_name: disease, "filename": filenames})
     with open(result_path, "w") as file:
         df.to_csv(file, sep="\t", index=False, header=True)
 
@@ -27,6 +27,8 @@ def convert_metadata(path, result_path, extension: str = "csv"):
 if __name__ == "__main__":
     path = sys.argv[1]
     result_path = sys.argv[2]
-    extension = sys.argv[3] if len(sys.argv) == 4 else "csv"
+    disease = sys.argv[3]
+    alternative = sys.argv[4]
+    extension = sys.argv[5] if len(sys.argv) == 6 else "csv"
 
-    convert_metadata(path, result_path, extension)
+    convert_metadata(path, result_path, disease, alternative, extension)
