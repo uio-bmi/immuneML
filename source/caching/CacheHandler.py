51fd3332d6ec5ebbe039bce82dab0c531d6e7161
pavlovicmilena <milenapavlovic775@gmail.com>
2019-06-21 13:51:01 +0200

    add memo and utility fns to cache handler

--- source/caching/CacheHandler.py
+++ source/caching/CacheHandler.py
@@ -9,8 +9,8 @@ class CacheHandler:
 
     @staticmethod
     def get(params: tuple):
-        h = str(hash(params)).replace("-", "_")
-        filename = "{}{}.pickle".format(EnvironmentSettings.cache_path, h)
+        h = CacheHandler._hash(params)
+        filename = CacheHandler._build_filename(h)
 
         obj = None
         if os.path.isfile(filename):
@@ -20,8 +20,43 @@ class CacheHandler:
         return obj
 
     @staticmethod
+    def get_by_key(cache_key: str):
+        filename = CacheHandler._build_filename(cache_key)
+        obj = None
+        if os.path.isfile(filename):
+            with open(filename, "rb") as file:
+                obj = pickle.load(file)
+        return obj
+
+    @staticmethod
+    def _build_filename(cache_key: str):
+        return "{}{}.pickle".format(EnvironmentSettings.cache_path, cache_key)
+
+    @staticmethod
     def add(params: tuple, caching_object):
         PathBuilder.build(EnvironmentSettings.cache_path)
-        h = str(hash(params)).replace("-", "_")
-        with open("{}{}.pickle".format(EnvironmentSettings.cache_path, h), "wb") as file:
+        h = CacheHandler._hash(params)
+        with open(CacheHandler._build_filename(h), "wb") as file:
             pickle.dump(caching_object, file)
+
+    @staticmethod
+    def add_by_key(cache_key: str, caching_object):
+        PathBuilder.build(EnvironmentSettings.cache_path)
+        with open(CacheHandler._build_filename(cache_key), "wb") as file:
+            pickle.dump(caching_object, file)
+
+    @staticmethod
+    def generate_cache_key(params: tuple, suffix: str):
+        return CacheHandler._hash(params) + suffix
+
+    @staticmethod
+    def memo(cache_key: str, fn):
+        result = CacheHandler.get_by_key(cache_key)
+        if result is None:
+            result = fn()
+            CacheHandler.add_by_key(cache_key, result)
+        return result
+
+    @staticmethod
+    def _hash(params: tuple) -> str:
+        return str(hash(params)).replace("-", "_")
--- test/caching/test_cacheHandler.py
+++ test/caching/test_cacheHandler.py
@@ -21,16 +21,50 @@ class TestCacheHandler(TestCase):
         self.assertEqual(obj, obj2)
         os.remove(filename)
 
+    def test_get_by_key(self):
+        params = (("k1", 1), ("k2", 2))
+        obj = "object_example"
+
+        h = CacheHandler._hash(params)
+        filename = CacheHandler._build_filename(h)
+        with open(filename, "wb") as file:
+            pickle.dump(obj, file)
+
+        obj2 = CacheHandler.get_by_key(h)
+        self.assertEqual(obj, obj2)
+        os.remove(filename)
+
     def test_add(self):
         params = ("k1", 1), ("k2", ("k3", 2))
         obj = "object_example"
 
         CacheHandler.add(params, obj)
 
-        h = str(hash(params)).replace("-", "_")
-        filename = "{}{}.pickle".format(EnvironmentSettings.cache_path, h)
+        h = CacheHandler._hash(params)
+        filename = CacheHandler._build_filename(h)
         with open(filename, "rb") as file:
             obj2 = pickle.load(file)
 
         self.assertEqual(obj, obj2)
         os.remove(filename)
+
+    def test_add_by_key(self):
+        params = ("k1", 1), ("k2", ("k3", 2))
+        obj = "object_example"
+
+        h = CacheHandler._hash(params)
+        CacheHandler.add_by_key(h, obj)
+        filename = CacheHandler._build_filename(h)
+        with open(filename, "rb") as file:
+            obj2 = pickle.load(file)
+
+        self.assertEqual(obj, obj2)
+        os.remove(filename)
+
+    def test_memo(self):
+        fn = lambda: "abc"
+        cache_key = "a123"
+        obj = CacheHandler.memo(cache_key, fn)
+        self.assertEqual("abc", obj)
+
+        os.remove(CacheHandler._build_filename(cache_key))
