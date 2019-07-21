30aaae87b6a6dcda47720cf6568227ab7ca2e1e1
pavlovicmilena <milenapavlovic775@gmail.com>
2019-07-22 00:13:49 +0200

    add test placeholders for hp optimization process

---
+++ source/workflows/processes/test_HPOptimizationProcess.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestHPOptimizationProcess(TestCase):
+    def test_run_outer_cv(self):
+        self.fail()
+
+    def test_run_outer_fold(self):
+        self.fail()
+
+    def test_run_inner_cv(self):
+        self.fail()
+
+    def test_run_inner_fold(self):
+        self.fail()
+
+    def test_test_hp_setting(self):
+        self.fail()
+
+    def test_run_setting(self):
+        self.fail()
