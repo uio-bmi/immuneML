42fb4e4e327aa89ca5894a8e2f324858d209a249
pavlovicmilena <milenapavlovic775@gmail.com>
2019-09-04 15:44:29 +0200

    add custom css for read the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,6 +94,10 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    "css/immuneml.css",
+]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
