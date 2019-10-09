6d681993442172a3cc54ec02dd2d2f76bd7d7b6c
pavlovicmilena <milenapavlovic775@gmail.com>
2019-10-09 15:41:34 +0200

    reset index in the metadata when filtering

--- source/preprocessing/filters/Filter.py
+++ source/preprocessing/filters/Filter.py
@@ -13,6 +13,7 @@ class Filter(Preprocessor, ABC):
     def build_new_metadata(dataset: RepertoireDataset, indices_to_keep: list, result_path: str):
         if dataset.metadata_file:
             df = pd.read_csv(dataset.metadata_file).iloc[indices_to_keep, :]
+            df.reset_index(drop=True, inplace=True)
             for index, row in df.iterrows():
                 row["filename"] = dataset.get_filenames()[index]
             path = result_path + "/{}_metadata_filtered.csv" \
