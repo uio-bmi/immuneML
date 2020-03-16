d99bec8ce1da9d1ec3557f3af4dc3c5159e1e7d0
pavlovicmilena <milenapavlovic775@gmail.com>
2020-03-16 14:38:22 +0100

    add meaningful error message during parsing

--- source/logging/Logger.py
+++ source/logging/Logger.py
@@ -15,7 +15,13 @@ def log(func):
                 except Exception as e:
                     if EnvironmentSettings.log_level == LogLevel.DEBUG:
                         print('\n\n%s --- Exception in %s : %s\n\n' % (datetime.datetime.now(), func.__name__, e))
-                    raise e
+                    if "dsl" in func.__globals__["__name__"]:
+                        raise Exception(f"{e}\n\n"
+                                        f"ImmuneMLParser: an error occurred during parsing in function {func.__name__} "
+                                        f" with parameters: {args}.\n\nFor more details on how to write the specification, "
+                                        f"see the documentation. For technical description of the error, see the log above.")
+                    else:
+                        raise e
             finally:
                 print("%s --- Exiting: %s" % (datetime.datetime.now(), func.__name__))
         else:
