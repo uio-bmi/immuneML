b3467346089cc74778bba9c6ca1bf487b75f0da1
pavlovicmilena <milenapavlovic775@gmail.com>
2020-04-24 19:21:43 +0200

    change script in setup.py to immuneMLApp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="immune-ml",
-    version="0.0.1",
+    version="0.0.2",
     description="immuneML is a software platform for machine learning analysis of immune receptor sequences",
     long_description=open("README.md").read(),
     author="Milena Pavlovic",
@@ -15,10 +15,10 @@ setup(
         "Programming Language :: Python :: 3"
     ],
     python_requires='>=3.6',
-    packages = find_packages(),
+    packages=find_packages(),
     entry_points={
-          'console_scripts': [
-              'immune-ml = source.workflows.instructions.quickstart:main'
-          ]
-      },
+        'console_scripts': [
+            'immune-ml = source.app.ImmuneMLApp:main'
+        ]
+    },
 )
