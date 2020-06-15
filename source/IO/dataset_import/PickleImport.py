1b0b27f7db788bc48384b8c7eaa073d22b42bcc0
pavlovicmilena <milenapavlovic775@gmail.com>
2020-06-15 14:30:45 +0200

    update repertoire paths for galaxy tool

--- source/IO/dataset_import/PickleImport.py
+++ source/IO/dataset_import/PickleImport.py
@@ -2,9 +2,11 @@
 
 import os
 import pickle
+from glob import glob
 
 from source.IO.dataset_import.DataImport import DataImport
 from source.IO.dataset_import.DatasetImportParams import DatasetImportParams
+from source.data_model.dataset.Dataset import Dataset
 from source.data_model.dataset.RepertoireDataset import RepertoireDataset
 
 
@@ -19,7 +21,7 @@ class PickleImport(DataImport):
     """
 
     @staticmethod
-    def import_dataset(params: dict, dataset_name: str) -> RepertoireDataset:
+    def import_dataset(params: dict, dataset_name: str) -> Dataset:
         pickle_params = DatasetImportParams.build_object(**params)
         assert os.path.isfile(pickle_params.path), "PickleImport: the dataset file does not exist in the given path: " + pickle_params.path
         with open(pickle_params.path, "rb") as file:
@@ -28,4 +30,28 @@ class PickleImport(DataImport):
         if pickle_params.metadata_file is not None and hasattr(dataset, "metadata_file"):
             dataset.metadata_file = pickle_params.metadata_file
 
+        if isinstance(dataset, RepertoireDataset):
+            dataset = PickleImport._update_repertoire_paths(pickle_params, dataset)
+
         return dataset
+
+    @staticmethod
+    def _update_repertoire_paths(pickle_params, dataset):
+        path = PickleImport._discover_repertoire_path(pickle_params, dataset)
+        if path is not None:
+            for repertoire in dataset.repertoires:
+                repertoire.data_filename = f"{path}{os.path.basename(repertoire.data_filename)}"
+                repertoire.metadata_filename = f"{path}{os.path.basename(repertoire.metadata_filename)}"
+        return dataset
+
+    @staticmethod
+    def _discover_repertoire_path(pickle_params, dataset):
+        dataset_dir = os.path.dirname(pickle_params.path)
+        if len(list(glob(f"{dataset_dir}/*.npy"))) == len(dataset.repertoires):
+            path = dataset_dir + "/"
+        elif len(list(glob(f"{dataset_dir}/repertoires/*.npy"))) == len(dataset.repertoires):
+            path = dataset_dir + "/repertoires/"
+        else:
+            path = None
+
+        return path
