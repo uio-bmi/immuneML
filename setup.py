a5475b0875fcc470daab29a0478f78b1687cf7d0
pavlovicmilena <milenapavlovic775@gmail.com>
2020-07-09 10:43:15 +0200

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="immune-ml",
-    version="0.0.73",
+    version="0.0.74",
     description="immuneML is a software platform for machine learning analysis of immune receptor sequences",
     long_description=open("README.md").read(),
     author="Milena Pavlovic",
@@ -20,7 +20,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3"
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     packages=find_packages(exclude=["test", "test.*", "performance_tests", "performance_tests.*"]),
     package_data={
         'source': ['IO/dataset_import/conversion/*.csv', "presentation/html/templates/*.html", "presentation/html/templates/css/*.css",
