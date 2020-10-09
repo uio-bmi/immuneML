e033e5ef432233c1ab305eabf1182724ad0de50f
Lonneke Scheffer <lonsch96@live.nl>
2020-10-09 23:39:58 +0200

    updated build_dataset_yaml script according to refactoring, plus improved test

--- source/api/galaxy/build_dataset_yaml.py
+++ source/api/galaxy/build_dataset_yaml.py
@@ -3,6 +3,7 @@ import os
 import sys
 import yaml
 
+from source.data_model.receptor.RegionType import RegionType
 from source.util.PathBuilder import PathBuilder
 
 
@@ -25,9 +26,20 @@ def build_specs(args):
         }
     }
 
+    specs["definitions"]["datasets"][args.dataset_name]["params"]["region_type"] = RegionType.IMGT_CDR3.name
+    specs["definitions"]["datasets"][args.dataset_name]["params"]["result_path"] = "./"
+
     if args.metadata_file == "":
-        specs["definitions"]["datasets"][args.dataset_name]["params"]["path"] = "."
+        specs["definitions"]["datasets"][args.dataset_name]["params"]["path"] = "./"
+        specs["definitions"]["datasets"][args.dataset_name]["params"]["is_repertoire"] = False
+
+        paired = True if args.paired == "True" else False
+
+        specs["definitions"]["datasets"][args.dataset_name]["params"]["paired"] = paired
+        if paired:
+            specs["definitions"]["datasets"][args.dataset_name]["params"]["receptor_chains"] = args.receptor_chains
     else:
+        specs["definitions"]["datasets"][args.dataset_name]["params"]["is_repertoire"] = True
         specs["definitions"]["datasets"][args.dataset_name]["params"]["metadata_file"] = args.metadata_file
 
     return specs
@@ -38,6 +50,8 @@ def parse_commandline_arguments(args):
     parser = argparse.ArgumentParser(description="Tool for building immuneML defintion YAML for Galaxy Create Dataset tool")
     parser.add_argument("-r", "--format", required=True, help="The format of the repertoire/receptor dataset")
     parser.add_argument("-m", "--metadata_file", default="", help="The metadata file when using a repertoire dataset. When using a receptor dataset, you may supply an empty string.")
+    parser.add_argument("-p", "--paired", choices=["True", "False"], default="False", help="When the data is not repertoire data (metadata file = ''), this specifies whether the data is paired (ReceptorDataset) or unpaired (SequenceDataset)")
+    parser.add_argument("-c", "--receptor_chains", choices=["TRA_TRB", "TRG_TRD", "IGH_IGL", "IGH_IGK"], default="TRA_TRB", help="When the data is a ReceptorDataset, this specifies the type of receptor chains that are used.")
     parser.add_argument("-d", "--dataset_name", default="dataset", help="The name of the created dataset.")
 
     parser.add_argument("-o", "--output_path", required=True, help="Output location for the generated yaml file (directiory).")
