e99a5b98b77d7ab18123c928184f993d8fe8d373
pavlovicmilena <milenapavlovic775@gmail.com>
2020-11-12 13:28:37 +0100

    fix auroc when no probabilistic estimate is available

--- source/ml_metrics/ml_metrics.py
+++ source/ml_metrics/ml_metrics.py
@@ -16,4 +16,10 @@ def f1_score_macro(true_y, predicted_y):
 
 def roc_auc_score(true_y, predicted_y):
     predictions = np.array(predicted_y) if not isinstance(predicted_y, np.ndarray) else predicted_y
-    return metrics.roc_auc_score(true_y, predictions[:, 1] if len(predictions.shape) == 2 else predictions)
+    true_values = np.array(true_y) if not isinstance(true_y, np.ndarray) else true_y
+    if predictions.shape == true_values.shape:
+        return metrics.roc_auc_score(true_values, predictions)
+    elif len(predictions.shape) == 2 and predictions.shape[1] == 2:
+        return metrics.roc_auc_score(true_values, predictions[:, 1])
+    else:
+        return -1
