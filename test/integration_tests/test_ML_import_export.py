6fbfd38056fa72fe1cacdba345b8660469961efc
Lonneke Scheffer <lonsch96@live.nl>
2021-01-28 09:51:47 +0100

    fix test

--- test/integration_tests/test_ML_import_export.py
+++ test/integration_tests/test_ML_import_export.py
@@ -122,7 +122,7 @@ class TestMLIE(TestCase):
                     "dataset": "d1",
                     "config_path": str(path / "result_export/inst1/optimal_CD/zip/ml_model_CD.zip"),
                     "label": "CD",
-                    "pool_size": 4,
+                    "number_of_processes": 4,
                     "store_encoded_data": False
                 }
             },
