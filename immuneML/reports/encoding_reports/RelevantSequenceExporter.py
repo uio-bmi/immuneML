6d3a59a6a20e41428a16e8562ee06bb5546a421c
pavlovicmilena <milenapavlovic775@gmail.com>
2021-02-08 13:49:27 +0100

    remove SequenceCountEncoder.py & minor docs updates

--- immuneML/reports/encoding_reports/RelevantSequenceExporter.py
+++ immuneML/reports/encoding_reports/RelevantSequenceExporter.py
@@ -7,7 +7,6 @@ import pandas as pd
 from immuneML.data_model.dataset.RepertoireDataset import RepertoireDataset
 from immuneML.data_model.receptor.RegionType import RegionType
 from immuneML.encodings.filtered_sequence_encoding.SequenceAbundanceEncoder import SequenceAbundanceEncoder
-from immuneML.encodings.filtered_sequence_encoding.SequenceCountEncoder import SequenceCountEncoder
 from immuneML.reports.ReportOutput import ReportOutput
 from immuneML.reports.ReportResult import ReportResult
 from immuneML.reports.encoding_reports.EncodingReport import EncodingReport
@@ -16,7 +15,7 @@ from immuneML.util.PathBuilder import PathBuilder
 
 class RelevantSequenceExporter(EncodingReport):
     """
-    Exports the sequences that are extracted as label-associated using `SequenceAbundance` or `SequenceCount` encoders in AIRR-compliant format.
+    Exports the sequences that are extracted as label-associated using the `SequenceAbundance` encoder in AIRR-compliant format.
 
     Arguments: there are no arguments for this report.
 
@@ -69,7 +68,7 @@ class RelevantSequenceExporter(EncodingReport):
         return {**RelevantSequenceExporter.COLUMN_MAPPING, **column_mapping}
 
     def check_prerequisites(self):
-        valid_encodings = [SequenceAbundanceEncoder.__name__, SequenceCountEncoder.__name__]
+        valid_encodings = [SequenceAbundanceEncoder.__name__]
         if self.dataset.encoded_data is None or self.dataset.encoded_data.info is None:
             logging.warning("RelevantSequenceExporter: the dataset is not encoded, skipping this report...")
             return False
