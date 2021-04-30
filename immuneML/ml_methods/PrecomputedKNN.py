a413ff07cee1d69e8a0d985cb754a7132b6b3b03
Lonneke Scheffer <lonsch96@live.nl>
2021-04-30 14:46:56 +0200

    Added PrecomputedKNN for use with distance-based encoders

--- immuneML/ml_methods/PrecomputedKNN.py
+++ immuneML/ml_methods/PrecomputedKNN.py
@@ -4,16 +4,17 @@ from immuneML.ml_methods.SklearnMethod import SklearnMethod
 from scripts.specification_util import update_docs_per_mapping
 
 
-class KNN(SklearnMethod):
+class PrecomputedKNN(SklearnMethod):
     """
     This is a wrapper of scikit-learn’s KNeighborsClassifier class.
-    This ML method creates a distance matrix using the given encoded data. If the encoded data is already a distance
-    matrix (for example, when using the :ref:`Distance` or :ref:`HammingDistance` encoders), please use :ref:`PrecomputedKNN` instead.
+    This ML method takes a pre-computed distance matrix, as created by the :ref:`Distance` or :ref:`HammingDistance` encoders.
+    If you would like to use a different encoding in combination with KNN, please use :ref:`KNN` instead.
 
     Please see the `scikit-learn documentation <https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html>`_
     of KNeighborsClassifier for the parameters.
 
     For usage instructions, check :py:obj:`~immuneML.ml_methods.SklearnMethod.SklearnMethod`.
+
 
     YAML specification:
 
@@ -37,15 +38,18 @@ class KNN(SklearnMethod):
 
     """
 
+    # todo raise error if the given encoded data is not a distance matrix?
+
     def __init__(self, parameter_grid: dict = None, parameters: dict = None):
         parameters = parameters if parameters is not None else {}
         parameter_grid = parameter_grid if parameter_grid is not None else {}
 
-        super(KNN, self).__init__(parameter_grid=parameter_grid, parameters=parameters)
+        super(PrecomputedKNN, self).__init__(parameter_grid=parameter_grid, parameters=parameters)
 
     def _get_ml_model(self, cores_for_training: int = 2, X=None):
         params = self._parameters
         params["n_jobs"] = cores_for_training
+        params["metric"] = "precomputed"
         return KNeighborsClassifier(**params)
 
     def get_params(self):
@@ -56,7 +60,7 @@ class KNN(SklearnMethod):
 
     @staticmethod
     def get_documentation():
-        doc = str(KNN.__doc__)
+        doc = str(PrecomputedKNN.__doc__)
 
         mapping = {
             "For usage instructions, check :py:obj:`~immuneML.ml_methods.SklearnMethod.SklearnMethod`.": SklearnMethod.get_usage_documentation("KNN"),
