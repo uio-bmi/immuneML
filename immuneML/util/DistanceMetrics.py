6e8439dd2f889563d91c8ec05176fbefe668b458
Lonneke Scheffer <lonsch96@live.nl>
2021-10-13 18:41:09 +0200

    Added new distance metric type MorisitaHorn

--- immuneML/util/DistanceMetrics.py
+++ immuneML/util/DistanceMetrics.py
@@ -3,3 +3,9 @@ import numpy as np
 
 def jaccard(vector1, vector2, tmp_vector=None):
     return 1 - np.sum(np.logical_and(vector1, vector2, out=tmp_vector)) / np.sum(np.logical_or(vector1, vector2, out=tmp_vector))
+
+def morisita_horn(vector1, vector2, *args, **kwargs):
+    sum1 = np.sum(vector1)
+    sum2 = np.sum(vector2)
+
+    return 1 - ((2 * np.sum(vector1 * vector2)) / ((np.sum(vector1 ** 2) / (sum1 ** 2) + np.sum(vector2 ** 2) / (sum2**2)) * sum1 * sum2))
