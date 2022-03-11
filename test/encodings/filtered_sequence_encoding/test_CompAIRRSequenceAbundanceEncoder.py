9f8fddbfc37cae8a15a5854f7ccec619c5107b55
Lonneke Scheffer <lonsch96@live.nl>
2022-03-11 15:40:12 +0100

    remove obsolete import

--- test/encodings/filtered_sequence_encoding/test_CompAIRRSequenceAbundanceEncoder.py
+++ test/encodings/filtered_sequence_encoding/test_CompAIRRSequenceAbundanceEncoder.py
@@ -9,7 +9,6 @@ import pandas as pd
 from immuneML.caching.CacheType import CacheType
 from immuneML.data_model.dataset.RepertoireDataset import RepertoireDataset
 from immuneML.encodings.EncoderParams import EncoderParams
-from immuneML.encodings.abundance_encoding.AbundanceEncoderHelper import AbundanceEncoderHelper
 from immuneML.encodings.abundance_encoding.CompAIRRSequenceAbundanceEncoder import CompAIRRSequenceAbundanceEncoder
 from immuneML.environment.Constants import Constants
 from immuneML.environment.EnvironmentSettings import EnvironmentSettings
