43e54cbdff89174ad9fec2fc99e9ce504c906cb9
pavlovicmilena <milenapavlovic775@gmail.com>
2022-05-13 11:26:05 +0200

    allow in mixcr to import nt only or aa only

--- immuneML/IO/dataset_import/MiXCRImport.py
+++ immuneML/IO/dataset_import/MiXCRImport.py
@@ -6,6 +6,7 @@ from immuneML.data_model.dataset import Dataset
 from immuneML.data_model.receptor.RegionType import RegionType
 from immuneML.data_model.repertoire.Repertoire import Repertoire
 from immuneML.util.ImportHelper import ImportHelper
+from immuneML.util.ParameterValidator import ParameterValidator
 from scripts.specification_util import update_docs_per_mapping
 
 
@@ -21,58 +22,32 @@ class MiXCRImport(DataImport):
 
         path (str): This is the path to a directory with MiXCR files to import. By default path is set to the current working directory.
 
-        is_repertoire (bool): If True, this imports a RepertoireDataset. If False, it imports a SequenceDataset.
-        By default, is_repertoire is set to True.
+        is_repertoire (bool): If True, this imports a RepertoireDataset. If False, it imports a SequenceDataset. By default, is_repertoire is set to True.
 
-        metadata_file (str): Required for RepertoireDatasets. This parameter specifies the path to the metadata file.
-        This is a csv file with columns filename, subject_id and arbitrary other columns which can be used as labels in instructions.
-        Only the MiXCR files included under the column 'filename' are imported into the RepertoireDataset.
-        For setting SequenceDataset metadata, metadata_file is ignored, see metadata_column_mapping instead.
+        metadata_file (str): Required for RepertoireDatasets. This parameter specifies the path to the metadata file. This is a csv file with columns filename, subject_id and arbitrary other columns which can be used as labels in instructions. Only the MiXCR files included under the column 'filename' are imported into the RepertoireDataset. For setting SequenceDataset metadata, metadata_file is ignored, see metadata_column_mapping instead.
 
-        import_illegal_characters (bool): Whether to import sequences that contain illegal characters, i.e., characters
-        that do not appear in the sequence alphabet (amino acids including stop codon '*', or nucleotides). When set to false, filtering is only
-        applied to the sequence type of interest (when running immuneML in amino acid mode, only entries with illegal
-        characters in the amino acid sequence, such as '_', are removed). By default import_illegal_characters is False.
+        import_illegal_characters (bool): Whether to import sequences that contain illegal characters, i.e., characters that do not appear in the sequence alphabet (amino acids including stop codon '*', or nucleotides). When set to false, filtering is only applied to the sequence type of interest (when running immuneML in amino acid mode, only entries with illegal characters in the amino acid sequence, such as '_', are removed). By default import_illegal_characters is False.
 
-        import_empty_nt_sequences (bool): imports sequences which have an empty nucleotide sequence field; can be True or False.
-        By default, import_empty_nt_sequences is set to True.
+        import_empty_nt_sequences (bool): imports sequences which have an empty nucleotide sequence field; can be True or False. By default, import_empty_nt_sequences is set to True.
 
-        import_empty_aa_sequences (bool): imports sequences which have an empty amino acid sequence field; can be True or False; for analysis on
-        amino acid sequences, this parameter should be False (import only non-empty amino acid sequences). By default, import_empty_aa_sequences is set to False.
+        import_empty_aa_sequences (bool): imports sequences which have an empty amino acid sequence field; can be True or False; for analysis on amino acid sequences, this parameter should be False (import only non-empty amino acid sequences). By default, import_empty_aa_sequences is set to False.
 
-        region_type (str): Which part of the sequence to import. By default, this value is set to IMGT_CDR3. This means the
-        first and last amino acids are removed from the CDR3 sequence, as MiXCR uses IMGT junction as CDR3.
-        Alternatively to importing the CDR3 sequence, other region types can be specified here as well.
-        Valid values for region_type are defined in MiXCRImport.SEQUENCE_NAME_MAP.
+        region_type (str): Which part of the sequence to import. By default, this value is set to IMGT_CDR3. This means the first and last amino acids are removed from the CDR3 sequence, as MiXCR uses IMGT junction as CDR3. Alternatively to importing the CDR3 sequence, other region types can be specified here as well. Valid values for region_type are defined in MiXCRImport.SEQUENCE_NAME_MAP.
 
-        column_mapping (dict): A mapping from MiXCR column names to immuneML's internal data representation.
-        For MiXCR, this is by default set to:
+        column_mapping (dict): A mapping from MiXCR column names to immuneML's internal data representation. The columns that specify the sequences to import are handled by the region_type parameter. A custom column mapping can be specified here if necessary (for example; adding additional data fields if they are present in the MiXCR file, or using alternative column names). Valid immuneML fields that can be specified here are defined by Repertoire.FIELDS. For MiXCR, this is by default set to:
 
-        .. indent with spaces
-        .. code-block:: yaml
+            .. indent with spaces
+            .. code-block:: yaml
 
-                cloneCount: counts
-                allVHitsWithScore: v_alleles
-                allJHitsWithScore: j_alleles
+                    cloneCount: counts
+                    allVHitsWithScore: v_alleles
+                    allJHitsWithScore: j_alleles
 
-        The columns that specify the sequences to import are handled by the region_type parameter.
-        A custom column mapping can be specified here if necessary (for example; adding additional data fields if
-        they are present in the MiXCR file, or using alternative column names).
-        Valid immuneML fields that can be specified here are defined by Repertoire.FIELDS
+        column_mapping_synonyms (dict): This is a column mapping that can be used if a column could have alternative names. The formatting is the same as column_mapping. If some columns specified in column_mapping are not found in the file, the columns specified in column_mapping_synonyms are instead attempted to be loaded. For MiXCR format, there is no default column_mapping_synonyms.
 
-        column_mapping_synonyms (dict): This is a column mapping that can be used if a column could have alternative names.
-        The formatting is the same as column_mapping. If some columns specified in column_mapping are not found in the file,
-        the columns specified in column_mapping_synonyms are instead attempted to be loaded.
-        For MiXCR format, there is no default column_mapping_synonyms.
+        columns_to_load (list): Specifies which subset of columns must be loaded from the MiXCR file. By default, this is: [cloneCount, allVHitsWithScore, allJHitsWithScore, aaSeqCDR3, nSeqCDR3]
 
-        columns_to_load (list): Specifies which subset of columns must be loaded from the MiXCR file. By default, this is:
-        [cloneCount, allVHitsWithScore, allJHitsWithScore, aaSeqCDR3, nSeqCDR3]
-
-        metadata_column_mapping (dict): Specifies metadata for SequenceDatasets. This should specify a mapping similar
-        to column_mapping where keys are MiXCR column names and values are the names that are internally used in immuneML
-        as metadata fields. These metadata fields can be used as prediction labels for SequenceDatasets.
-        For MiXCR format, there is no default metadata_column_mapping.
-        For setting RepertoireDataset metadata, metadata_column_mapping is ignored, see metadata_file instead.
+        metadata_column_mapping (dict): Specifies metadata for SequenceDatasets. This should specify a mapping similar to column_mapping where keys are MiXCR column names and values are the names that are internally used in immuneML as metadata fields. These metadata fields can be used as prediction labels for SequenceDatasets. For MiXCR format, there is no default metadata_column_mapping. For setting RepertoireDataset metadata, metadata_column_mapping is ignored, see metadata_file instead.
 
         separator (str): Column separator, for MiXCR this is by default "\\t".
 
@@ -135,15 +110,25 @@ class MiXCRImport(DataImport):
 
         Arguments:
 
-            metadata: the corresponding row from the metadata file with metadata such as subject_id, age, HLA or other info given there
+            df: the dataframe as imported from the csv file
+
             params: DatasetImportParams object defining what to import and how to do it
 
         Returns:
-            data frame corresponding to Repertoire.FIELDS and custom lists which can be used to create a Repertoire object
+
+            dataframe corresponding to Repertoire.FIELDS and custom lists which can be used to create a Repertoire object
 
         """
-        df["sequence_aas"] = df[MiXCRImport.SEQUENCE_NAME_MAP[params.region_type]["AA"]]
-        df["sequences"] = df[MiXCRImport.SEQUENCE_NAME_MAP[params.region_type]["NT"]]
+        aa_column = MiXCRImport.SEQUENCE_NAME_MAP[params.region_type]["AA"]
+        nt_column = MiXCRImport.SEQUENCE_NAME_MAP[params.region_type]["NT"]
+
+        ParameterValidator.assert_any_value_present(params.columns_to_load, [aa_column, nt_column], MiXCRImport.__name__, 'columns_to_load')
+
+        if aa_column in params.columns_to_load:
+            df["sequence_aas"] = df[aa_column]
+        if nt_column in params.columns_to_load:
+            df["sequences"] = df[nt_column]
+
         ImportHelper.junction_to_cdr3(df, params.region_type)
         df.loc[:, "region_types"] = params.region_type.name
 
