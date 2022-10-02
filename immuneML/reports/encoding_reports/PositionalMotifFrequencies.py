3e047512e498788b8aa269d719cb712a3c56a115
Eric Reber <103672622+EricEReber@users.noreply.github.com>
2022-10-02 21:21:41 +0200

    Implement motif continuity plots

--- immuneML/reports/encoding_reports/PositionalMotifFrequencies.py
+++ immuneML/reports/encoding_reports/PositionalMotifFrequencies.py
@@ -12,10 +12,13 @@ from immuneML.reports.ReportResult import ReportResult
 from immuneML.reports.encoding_reports.EncodingReport import EncodingReport
 from immuneML.util.PathBuilder import PathBuilder
 
-# todo maybe think of a better (more general) name if this is going to contain many motif reports
 class PositionalMotifFrequencies(EncodingReport):
     """
+    Plots a stacked bar plot of amino acid occurrence at different indices in any given dataset, along with a plot
+    investigating motif continuity which displays a bar plot of the distances between the amino acids in the motifs in
+    the given dataset. Note that a distance of 1 means that the amino acids are continuous (next to each other).
 
+    # todo write args, YAML specification and other information that might be needed
 
     """
 
@@ -38,41 +41,63 @@ class PositionalMotifFrequencies(EncodingReport):
         PathBuilder.build(self.result_path)
 
         motifs = self.dataset.encoded_data.feature_names
-        plotting_data = self._get_plotting_data(motifs)
+        stacked_bar, hist_dict = self._get_plotting_data(motifs)
 
-        report_output_fig = self._safe_plot(stacked_bar=plotting_data)
-        output_figures = None if report_output_fig is None else [report_output_fig]
+        output_figures = self._safe_plot(stacked_bar=stacked_bar, hist_dict=hist_dict)
         return ReportResult(name=self.name, output_figures=output_figures)
+
 
     def _get_plotting_data(self, motifs):
         positional_aa_counts = {aa: [0 for i in range(self.get_sequence_length())]
                                 for aa in EnvironmentSettings.get_sequence_alphabet(SequenceType.AMINO_ACID)}
 
+        distance_aa_counts = {position: {distance: 0 for distance in range(1, self.get_sequence_length())} for position in range(self.get_sequence_length())}
+
         for motif in motifs:
             indices, amino_acids = PositionalMotifHelper.string_to_motif(motif, "&", "-")
+            distance = len(indices)
+
+            for i in range(distance-1):
+                distance_aa_counts[distance][indices[i+1]-indices[i]] += 1
 
             for index, amino_acid in zip(indices, amino_acids):
                 positional_aa_counts[amino_acid][index] += 1
 
+        hist_dict = dict()
+
+        for distance_count in distance_aa_counts:
+            if sum(distance_aa_counts[distance_count].values()) != 0:
+                hist_dict[distance_count] = distance_aa_counts[distance_count]
+
         stacked_bar = pd.DataFrame(positional_aa_counts)
         stacked_bar = stacked_bar.loc[:, (stacked_bar != 0).any(axis=0)]
 
-        return stacked_bar
+
+        return stacked_bar, hist_dict
 
     def _get_color_discrete_sequence(self):
         return px.colors.qualitative.Pastel[:-1] + px.colors.qualitative.Set3
 
-    def _plot(self, stacked_bar) -> ReportOutput:
+    def _plot(self, stacked_bar, hist_dict) -> ReportOutput:
         file_path = self.result_path / f"positional_motif_frequencies.html"
-
-        fig = px.bar(stacked_bar,
+        ReportOutputs = []
+        stacked_bar_fig = px.bar(stacked_bar,
                      labels={"index": "Amino acid position", "value": "Frequency across high-scoring motifs"},
                      text="variable", color_discrete_sequence=self._get_color_discrete_sequence(), template='plotly_white')
-        fig.update_layout(showlegend=False, font={"size": 14}, xaxis={"tickmode": "linear"})
-        fig.write_html(str(file_path))
+        stacked_bar_fig.update_layout(showlegend=False, font={"size": 14}, xaxis={"tickmode": "linear"})
+        stacked_bar_fig.write_html(str(file_path))
+        ReportOutputs.append(ReportOutput(path=file_path, name=f"Frequencies of amino acids found in the high-precision high-recall motifs"))
 
-        return ReportOutput(path=file_path, name=f"Frequencies of amino acids found in the high-precision high-recall motifs")
+        for position in hist_dict:
+            data = hist_dict[position]
+            file_path = self.result_path / f"motif_continuity_{position}_positions.html"
+            hist_fig = px.bar(x=data.keys(), y=data.values(), labels={"x":"Distance between Amino Acids", "y":"Distance occurrence"}, color_discrete_sequence=self._get_color_discrete_sequence(), template='plotly_white', title=f"Distances between Amino Acids for {position} positions")
+            hist_fig.update_layout(showlegend=False, font={"size": 14}, xaxis={"tickmode": "linear"})
+            hist_fig.write_html(str(file_path))
 
+            ReportOutputs.append(ReportOutput(path=file_path, name=f"Distance between amino acids in high-precision high-recall motifs"))
+
+        return ReportOutputs
 
 
 # todo add tests under ImmuneML/test/reports/encoding_reports/test_<name_of_class>.py, see other tests in that folder as examples
