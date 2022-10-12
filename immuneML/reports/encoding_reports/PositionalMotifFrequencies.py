460bb6e5eda15415a66f172e2851d37ffe988988
Eric Reber <103672622+EricEReber@users.noreply.github.com>
2022-10-12 17:43:40 +0200

    add refactor and tables

--- immuneML/reports/encoding_reports/PositionalMotifFrequencies.py
+++ immuneML/reports/encoding_reports/PositionalMotifFrequencies.py
@@ -4,7 +4,9 @@ import plotly.express as px
 import pandas as pd
 
 from immuneML.data_model.dataset.RepertoireDataset import RepertoireDataset
-from immuneML.encodings.motif_encoding.PositionalMotifHelper import PositionalMotifHelper
+from immuneML.encodings.motif_encoding.PositionalMotifHelper import (
+    PositionalMotifHelper,
+)
 from immuneML.environment.EnvironmentSettings import EnvironmentSettings
 from immuneML.environment.SequenceType import SequenceType
 from immuneML.reports.ReportOutput import ReportOutput
@@ -12,10 +14,11 @@ from immuneML.reports.ReportResult import ReportResult
 from immuneML.reports.encoding_reports.EncodingReport import EncodingReport
 from immuneML.util.PathBuilder import PathBuilder
 
+
 class PositionalMotifFrequencies(EncodingReport):
     """
     Plots a stacked bar plot of amino acid occurrence at different indices in any given dataset, along with a plot
-    investigating motif continuity which displays a bar plot of the distances between the amino acids in the motifs in
+    investigating motif continuity which displays a bar plot of the gap sizes between the amino acids in the motifs in
     the given dataset. Note that a distance of 1 means that the amino acids are continuous (next to each other).
 
     # todo write args, YAML specification and other information that might be needed
@@ -26,10 +29,19 @@ class PositionalMotifFrequencies(EncodingReport):
     def build_object(cls, **kwargs):
         return PositionalMotifFrequencies(**kwargs)
 
-    def __init__(self, dataset: RepertoireDataset = None, result_path: Path = None, name: str = None,
-                 number_of_processes: int = 1):
-        super().__init__(dataset=dataset, result_path=result_path,
-                         name=name, number_of_processes=number_of_processes)
+    def __init__(
+        self,
+        dataset: RepertoireDataset = None,
+        result_path: Path = None,
+        name: str = None,
+        number_of_processes: int = 1,
+    ):
+        super().__init__(
+            dataset=dataset,
+            result_path=result_path,
+            name=name,
+            number_of_processes=number_of_processes,
+        )
 
         self.name = name
 
@@ -41,63 +53,159 @@ class PositionalMotifFrequencies(EncodingReport):
         PathBuilder.build(self.result_path)
 
         motifs = self.dataset.encoded_data.feature_names
-        stacked_bar, hist_dict = self._get_plotting_data(motifs)
+        positional_aa_counts_df = self._get_positional_aa_counts(motifs)
+        gap_size_dict = self._get_gap_sizes(motifs)
 
-        output_figures = self._safe_plot(stacked_bar=stacked_bar, hist_dict=hist_dict)
-        return ReportResult(name=self.name, output_figures=output_figures)
+        positional_aa_counts_table = self._write_positional_aa_counts_table(
+            positional_aa_counts_df
+        )
+        gap_size_tables = self._write_gap_size_table(gap_size_dict)
 
+        output_figures = self._safe_plot(
+            positional_aa_counts_df=positional_aa_counts_df, gap_size_dict=gap_size_dict
+        )
+        return ReportResult(
+            name=self.name,
+            output_figures=output_figures,
+            output_tables=gap_size_tables + [positional_aa_counts_table],
+        )
 
-    def _get_plotting_data(self, motifs):
-        positional_aa_counts = {aa: [0 for i in range(self.get_sequence_length())]
-                                for aa in EnvironmentSettings.get_sequence_alphabet(SequenceType.AMINO_ACID)}
-
-        distance_aa_counts = {position: {distance: 0 for distance in range(1, self.get_sequence_length())} for position in range(self.get_sequence_length())}
+    def _get_gap_sizes(self, motifs):
+        gap_size_count = {
+            motif_size: {
+                gap_size: 0 for gap_size in range(1, self.get_sequence_length())
+            }
+            for motif_size in range(self.get_sequence_length())
+        }
 
         for motif in motifs:
-            indices, amino_acids = PositionalMotifHelper.string_to_motif(motif, "&", "-")
-            distance = len(indices)
+            indices, amino_acids = PositionalMotifHelper.string_to_motif(
+                motif, "&", "-"
+            )
+            indices_gap_size = len(indices)
 
-            for i in range(distance-1):
-                distance_aa_counts[distance][indices[i+1]-indices[i]] += 1
+            for i in range(indices_gap_size - 1):
+                gap_size_count[indices_gap_size][indices[i + 1] - indices[i]] += 1
+        gap_size_dict = dict()
+
+        for gap_size in gap_size_count:
+            if sum(gap_size_count[gap_size].values()) != 0:
+                gap_size_dict[gap_size] = gap_size_count[gap_size]
+
+        return gap_size_dict
+
+    def _get_positional_aa_counts(self, motifs):
+        positional_aa_counts = {
+            aa: [0 for i in range(self.get_sequence_length())]
+            for aa in EnvironmentSettings.get_sequence_alphabet(SequenceType.AMINO_ACID)
+        }
+
+        for motif in motifs:
+            indices, amino_acids = PositionalMotifHelper.string_to_motif(
+                motif, "&", "-"
+            )
 
             for index, amino_acid in zip(indices, amino_acids):
                 positional_aa_counts[amino_acid][index] += 1
 
-        hist_dict = dict()
+        positional_aa_counts_df = pd.DataFrame(positional_aa_counts)
+        positional_aa_counts_df = positional_aa_counts_df.loc[
+            :, (positional_aa_counts_df != 0).any(axis=0)
+        ]
 
-        for distance_count in distance_aa_counts:
-            if sum(distance_aa_counts[distance_count].values()) != 0:
-                hist_dict[distance_count] = distance_aa_counts[distance_count]
+        return positional_aa_counts_df
 
-        stacked_bar = pd.DataFrame(positional_aa_counts)
-        stacked_bar = stacked_bar.loc[:, (stacked_bar != 0).any(axis=0)]
+    def _plot_gap_sizes(self, gap_size_dict):
+        file_path = self.result_path / f"positional_motif_frequencies.html"
+        gap_size_figs = []
 
+        for motif_size in gap_size_dict:
+            data = gap_size_dict[motif_size]
+            file_path = self.result_path / f"gap_size_for_motif_size_{motif_size}.html"
+            gap_size_fig = px.bar(
+                x=data.keys(),
+                y=data.values(),
+                labels={
+                    "x": "Distance between Amino Acids",
+                    "y": "Distance occurrence",
+                },
+                color_discrete_sequence=self._get_color_discrete_sequence(),
+                template="plotly_white",
+                title=f"Distances between Amino Acids for {motif_size} positions",
+            )
+            gap_size_fig.update_layout(
+                showlegend=False, font={"size": 14}, xaxis={"tickmode": "linear"}
+            )
+            gap_size_fig.write_html(str(file_path))
 
-        return stacked_bar, hist_dict
+            gap_size_figs.append(
+                ReportOutput(
+                    path=file_path,
+                    name=f"Gap size between amino acids in high-precision high-recall motifs",
+                )
+            )
+
+        return gap_size_figs
+
+    def _plot_positional_aa_counts(self, positional_aa_counts_df):
+        file_path = self.result_path / f"positional_motif_frequencies.html"
+        positional_aa_counts_fig = px.bar(
+            positional_aa_counts_df,
+            labels={
+                "index": "Amino acid position",
+                "value": "Frequency across high-scoring motifs",
+            },
+            text="variable",
+            color_discrete_sequence=self._get_color_discrete_sequence(),
+            template="plotly_white",
+        )
+        positional_aa_counts_fig.update_layout(
+            showlegend=False, font={"size": 14}, xaxis={"tickmode": "linear"}
+        )
+        positional_aa_counts_fig.write_html(str(file_path))
+        return ReportOutput(
+            path=file_path,
+            name=f"Frequencies of amino acids found in the high-precision high-recall motifs",
+        )
 
     def _get_color_discrete_sequence(self):
         return px.colors.qualitative.Pastel[:-1] + px.colors.qualitative.Set3
 
-    def _plot(self, stacked_bar, hist_dict) -> ReportOutput:
-        file_path = self.result_path / f"positional_motif_frequencies.html"
-        ReportOutputs = []
-        stacked_bar_fig = px.bar(stacked_bar,
-                     labels={"index": "Amino acid position", "value": "Frequency across high-scoring motifs"},
-                     text="variable", color_discrete_sequence=self._get_color_discrete_sequence(), template='plotly_white')
-        stacked_bar_fig.update_layout(showlegend=False, font={"size": 14}, xaxis={"tickmode": "linear"})
-        stacked_bar_fig.write_html(str(file_path))
-        ReportOutputs.append(ReportOutput(path=file_path, name=f"Frequencies of amino acids found in the high-precision high-recall motifs"))
+    def _plot(self, positional_aa_counts_df, gap_size_dict) -> ReportOutput:
+        report_outputs = self._plot_gap_sizes(gap_size_dict)
+        report_outputs.append(self._plot_positional_aa_counts(positional_aa_counts_df))
 
-        for position in hist_dict:
-            data = hist_dict[position]
-            file_path = self.result_path / f"motif_continuity_{position}_positions.html"
-            hist_fig = px.bar(x=data.keys(), y=data.values(), labels={"x":"Distance between Amino Acids", "y":"Distance occurrence"}, color_discrete_sequence=self._get_color_discrete_sequence(), template='plotly_white', title=f"Distances between Amino Acids for {position} positions")
-            hist_fig.update_layout(showlegend=False, font={"size": 14}, xaxis={"tickmode": "linear"})
-            hist_fig.write_html(str(file_path))
+        return report_outputs if len(report_outputs) != 0 else None
 
-            ReportOutputs.append(ReportOutput(path=file_path, name=f"Distance between amino acids in high-precision high-recall motifs"))
+    def _write_positional_aa_counts_table(
+        self, positional_aa_counts_df
+    ) -> ReportOutput:
+        table_path = self.result_path / f"positional_aa_counts.csv"
+        positional_aa_counts_df.to_csv(table_path, index=True)
+        return ReportOutput(
+            path=table_path,
+            name=f"Frequencies of amino acids found in the high-precision high-recall motifs",
+        )
 
-        return ReportOutputs
+    def _write_gap_size_table(self, gap_size_dict) -> ReportOutput:
+        gap_size_tables = []
+        for motif_size in gap_size_dict:
+            table_path = (
+                self.result_path / f"gap_size_table_motif_size_{motif_size}.csv"
+            )
+            gap_size_sub_dict = gap_size_dict[motif_size]
+            gap_size_df = pd.DataFrame.from_dict(
+                gap_size_sub_dict, orient="index", dtype=str
+            )
+            gap_size_df.to_csv(table_path, index=True, header=["Gap size, occurance"])
+            gap_size_tables.append(
+                ReportOutput(
+                    path=table_path,
+                    name=f"Gap size between amino acids in high-precision high-recall motifs for motif size {motif_size}",
+                )
+            )
+
+        return gap_size_tables
 
 
 # todo add tests under ImmuneML/test/reports/encoding_reports/test_<name_of_class>.py, see other tests in that folder as examples
