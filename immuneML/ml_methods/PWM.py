c677847ea778af0dafb74a63b24b337db28fc90b
Theodor <theodor.bergersen@gmail.com>
2022-10-13 07:25:19 +0200

    Made PWM skeleton, added data generator and data modifier swapAcids.py

---
+++ immuneML/ml_methods/PWM.py
@@ -0,0 +1,46 @@
+
+from Bio import motifs
+from immuneML.ml_methods.GenerativeModel import GenerativeModel
+from scripts.specification_util import update_docs_per_mapping
+
+
+class PWM(GenerativeModel):
+
+    def get_classes(self) -> list:
+        pass
+
+    def __init__(self, parameter_grid: dict = None, parameters: dict = None):
+        parameters = parameters if parameters is not None else {}
+        parameter_grid = parameter_grid if parameter_grid is not None else {}
+
+        super(PWM, self).__init__(parameter_grid=parameter_grid, parameters=parameters)
+
+
+    def _get_ml_model(self, cores_for_training: int = 2, X=None):
+        self._motif = motifs.create(X)
+
+        params = self._parameters
+
+
+    def _fit(self, X, y, cores_for_training: int = 1):
+        print(self._motif)
+
+    def get_params(self):
+        return self.model.get_params(deep=True)
+
+    def can_predict_proba(self) -> bool:
+        raise Exception("can_predict_proba has not been implemented")
+
+    def get_compatible_encoders(self):
+        raise Exception("get_compatible_encoders has not been implemented")
+
+    @staticmethod
+    def get_documentation():
+        doc = str(PWM.__doc__)
+
+        mapping = {
+            "For usage instructions, check :py:obj:`~immuneML.ml_methods.SklearnMethod.SklearnMethod`.": GenerativeModel.get_usage_documentation("LSTM"),
+        }
+
+        doc = update_docs_per_mapping(doc, mapping)
+        return doc
