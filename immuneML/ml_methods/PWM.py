6a743de7d76470bdca0195af7e9f2b1a8132c690
Theodor <theodor.bergersen@gmail.com>
2022-10-17 21:42:50 +0200

    can make a PWM

--- immuneML/ml_methods/PWM.py
+++ immuneML/ml_methods/PWM.py
@@ -16,20 +16,22 @@ class PWM(GenerativeModel):
         super(PWM, self).__init__(parameter_grid=parameter_grid, parameters=parameters)
 
 
-    def _get_ml_model(self, cores_for_training: int = 2, X=None):
+    def _get_ml_model(self, cores_for_training: int = 2, X=None, dataset=None):
 
-        print(X.get_encoded_repertoire(X))
+        instances = [sequence.get_sequence() for repertoire in dataset.get_data() for sequence in repertoire.sequences]
 
-        self.model = motifs.create(X, alphabet="GPAVLIMCFYWHKRQNEDST")
+        self.model = motifs.create(instances, alphabet="GPAVLIMCFYWHKRQNEDST")
 
-        print(self.model)
         params = self._parameters
 
         return self.model
 
-    def _fit(self, X, y, cores_for_training: int = 1):
-        self.model = self._get_ml_model(X=X)
-        print("Her er motif", self.model)
+    def _fit(self, X, y, cores_for_training: int = 1, dataset=None):
+        self.model = self._get_ml_model(cores_for_training, X, dataset)
+        self.model.weblogo("mymotif.jpg")
+        pwm = self.model.counts.normalize(pseudocounts=0.5)
+        print(pwm)
+        return pwm
 
     def get_params(self):
         return self.model.get_params(deep=True)
