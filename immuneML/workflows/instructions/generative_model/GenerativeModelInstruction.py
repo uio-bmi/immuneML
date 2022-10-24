7def6a6f8ddf25ff599d41f8850f6e2f7894d172
Theodor <theodor.bergersen@gmail.com>
2022-10-24 11:28:41 +0200

    Near completed Generate for PWM

--- immuneML/workflows/instructions/generative_model/GenerativeModelInstruction.py
+++ immuneML/workflows/instructions/generative_model/GenerativeModelInstruction.py
@@ -45,6 +45,8 @@ class GenerativeModelInstruction(Instruction):
         encoded_dataset = self.encode(unit, result_path / "encoded_dataset")
         unit.report.dataset = encoded_dataset
         unit.genModel.fit(encoded_dataset.encoded_data, unit.label_config.get_label_objects()[0], dataset=unit.dataset)
+        unit.genModel.store(result_path)
+        unit.genModel.generate()
         unit.report.result_path = result_path / "report"
         report_result = unit.report.generate_report()
         return report_result
