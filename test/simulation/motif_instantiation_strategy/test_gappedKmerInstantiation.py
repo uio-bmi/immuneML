f663895e06cf1b8b7d2ab637e0658936d3f642e9
pavlovicmilena <milenapavlovic775@gmail.com>
2022-11-18 18:27:31 +0100

    simplify code in gapped kmer instantiation

--- test/simulation/motif_instantiation_strategy/test_gappedKmerInstantiation.py
+++ test/simulation/motif_instantiation_strategy/test_gappedKmerInstantiation.py
@@ -9,17 +9,17 @@ from immuneML.simulation.motif_instantiation_strategy.GappedKmerInstantiation im
 
 class TestGappedKmerInstantiation(TestCase):
 
+    alphabet_weights = {"T": 0.5, "F": 0.5, "A": 0, "C": 0, "D": 0, "E": 0, "G": 0, "H": 0, "K": 0, "I": 0, "L": 0, "M": 0,
+                        "N": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "V": 0, "W": 0, "Y": 0}
+
     def setUp(self) -> None:
         os.environ[Constants.CACHE_TYPE] = CacheType.TEST.name
 
     def test_instantiate_motif(self):
 
-        alphabet_weights = {"T": 0.5, "F": 0.5, "A": 0, "C": 0,  "D": 0, "E": 0, "G": 0, "H": 0, "K": 0, "I": 0, "L": 0, "M": 0,
-                            "N": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "V": 0, "W": 0, "Y": 0}
-
         strategy = GappedKmerInstantiation(hamming_distance_probabilities={0: 0.333, 1: 0.333, 2: 0.333}, max_gap=2, min_gap=1,
                                            position_weights={0: 1, 1: 1, 2: 0, 3: 0},
-                                           alphabet_weights=alphabet_weights)
+                                           alphabet_weights=self.alphabet_weights)
 
         instance = strategy.instantiate_motif("C/AS")
 
@@ -30,7 +30,7 @@ class TestGappedKmerInstantiation(TestCase):
 
         strategy = GappedKmerInstantiation(hamming_distance_probabilities={0: 0.5, 1: 0.5},
                                            position_weights={0: 1, 1: 0, 2: 0},
-                                           alphabet_weights=alphabet_weights)
+                                           alphabet_weights=self.alphabet_weights)
 
         instance = strategy.instantiate_motif("CAS")
 
@@ -41,17 +41,14 @@ class TestGappedKmerInstantiation(TestCase):
 
     def test_get_all_possible_motifs(self):
 
-        alphabet_weights = {"T": 0.5, "F": 0.5, "A": 0, "C": 0,  "D": 0, "E": 0, "G": 0, "H": 0, "K": 0, "I": 0, "L": 0, "M": 0,
-                            "N": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "V": 0, "W": 0, "Y": 0}
-
         strategy = GappedKmerInstantiation(hamming_distance_probabilities={0: 0.0, 1: 0.667, 2: 0.333}, max_gap=1, min_gap=0,
                                            position_weights={0: 1, 1: 1},
-                                           alphabet_weights=alphabet_weights)
+                                           alphabet_weights=self.alphabet_weights)
 
         instances = strategy.get_all_possible_instances("SQ", SequenceType.AMINO_ACID)
 
         self.assertListEqual(instances, ['[TF]Q', 'S[TF]', '[TF][TF]'])
 
-        strategy.position_weights = {0: 1, 1: 0, 2: 1}
+        strategy.position_weights = {0: 1, 1: 1, 2: 1}
         instances = strategy.get_all_possible_instances("S/Q", SequenceType.AMINO_ACID)
         self.assertListEqual(instances, ['[TF].{0,1}Q', 'S.{0,1}[TF]', '[TF].{0,1}[TF]'])
