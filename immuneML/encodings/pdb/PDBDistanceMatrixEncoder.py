ca1fe1716b1c1a1cff0065e582646269232a3e94
williasy <103262652+williasy@users.noreply.github.com>
2023-01-09 16:45:26 +0100

    Done refactoring PDBDistanceMatrixEncoder (Old name was PDBEncoder)

--- immuneML/encodings/pdb/PDBDistanceMatrixEncoder.py
+++ immuneML/encodings/pdb/PDBDistanceMatrixEncoder.py
@@ -25,217 +25,228 @@ class PDBDistanceMatrixEncoder(DatasetEncoder):
 
     def encode(self, dataset, params: EncoderParams):
 
-        structures =[]
-        CDRcounter = 0
+        processed_data = self.extract_data_for_encoding(dataset, params)
 
-        for parserObject in dataset.get_data():
-
-            collection = []
-            list = []
-
-            region = self.region_type
-            counter = 0
-
-            for model in parserObject:
-                for chain in model:
-                    residueCounter = 0
-                    for residue in chain:
-                        if residueCounter == 0:
-                            list.append(residue.get_parent())
-                            residueCounter = 1
-
-                        for atom in residue:
-                            if (atom.get_name() == "CA"):
-
-                                if "FULL_SEQUENCE" in region:
-                                    list.append(atom.get_coord())
-
-                                else:
-                                    if "IMGT_CDR3" in region and (isInCDR3(atom.full_id[3][1]) or len(collection) >= 2):
-                                        list.append(atom.get_coord())
-
-                                    if "IMGT_CDR2" in region and (isInCDR2(atom.full_id[3][1]) or len(collection) >= 2):
-                                        list.append(atom.get_coord())
-
-                                    if "IMGT_CDR1" in region and (isInCDR1(atom.full_id[3][1]) or len(collection) >= 2):
-                                        list.append(atom.get_coord())
-
-
-                    saveList = list.copy()
-                    collection.append(saveList)
-                    counter = counter + 1
-                    list.clear()
-
-            lightID = -1
-            heavyID = -1
-            antigenID = -1
-
-            for i in range(0,len(collection)):
-                chainID = collection[i][0].id
-
-                if 'L' in chainID or 'A' in chainID:
-                    lightID = i
-
-                elif 'H' in chainID or 'B' in chainID:
-                    heavyID = i
-
-                elif 'P' in chainID or 'C' in chainID:
-                    antigenID = i
-
-                collection[i].pop(0)
-
-
-            if lightID >= 0 and heavyID >= 0 and antigenID == -1:
-                if lightID + heavyID == 1:
-                    antigenID = 2
-
-                elif lightID + heavyID == 3:
-                    antigenID = 0
-
-            elif lightID == -1  or heavyID == -1 or antigenID == -1:
-                lightID = 0
-                heavyID = 1
-                antigenID = 2
-
-
-            lightChainDistanceToAntigen = np.zeros(shape=(len(collection[antigenID]), len(collection[lightID])))
-
-            caluculateDistance(collection, lightID, antigenID, lightChainDistanceToAntigen)
-
-            heavyChainDistanceToAntigen = np.zeros(shape=(len(collection[antigenID]), len(collection[heavyID])))
-            caluculateDistance(collection, heavyID, antigenID, heavyChainDistanceToAntigen)
-
-            paddedArrays = paddingOfArray(lightChainDistanceToAntigen,heavyChainDistanceToAntigen)
-
-            structures.append([paddedArrays[0],paddedArrays[1]])
-            CDRcounter = CDRcounter +1
-
-
-        longestAntigen = 0
-        longestChain = 0
-
-        for x in structures:
-            if len(x[0]) > longestAntigen:
-                longestAntigen = len(x[0])
-            if len(x[0][0]) > longestChain:
-                longestChain = len(x[0][0])
-
-        paddedStructures = []
-        for x in structures:
-            paddedStructures.append(expandAntibodyChainToMaxLength(x, longestAntigen, longestChain))
-
-
-
-        numpArray = np.array(paddedStructures)
-
-        encoded_dataset = PDBDataset(self, dataset.pdbFilePaths,dataset.labels,dataset.metadata_file,EncodedData(numpArray,None,dataset.get_example_ids()))
+        encoded_dataset = PDBDataset(self, dataset.pdbFilePaths,dataset.labels,dataset.metadata_file,EncodedData(processed_data,None,dataset.get_example_ids()))
 
         #print("Shape of encoded_dataset.encoded_data.examples: " , numpy.shape(encoded_dataset.encoded_data.examples))
         return encoded_dataset
 
 
-def caluculateDistance(collection, chain, antigen, list):
-    for x in range(len(collection[antigen])):
-        for i in range(len(collection[chain])):
-            a = np.array((collection[chain][i][0], collection[chain][i][1], collection[chain][i][2]))
-            b = np.array((collection[antigen][x][0], collection[antigen][x][1], collection[antigen][x][2]))
+    def extract_data_for_encoding(self, dataset, params: EncoderParams):
 
-            rounded = round(np.linalg.norm(a - b), 2)
-            list[x][i] = rounded
+        structures =[]
 
+        for parser_object in dataset.get_data():
+            collection = self.get_carbon_alphas(parser_object)
 
-"""
-This method takes in two 2D arrays as input. The format should be the light chain first and heavy second
-Then pads the shortest one to the length of the longest one
-It returns a tuple like (lightChain,HeavyChain)
+            chain_IDs = self.get_chain_ID(collection)
 
-The arrays should be numpy arrays.
-The arrays should have the same amount of rows, but not columns
-It is the individual rows that gets padded values to match the longer one
-
-"""
-def paddingOfArray(lightChain, heavyChain):
-    diff = len(lightChain[0]) - len(heavyChain[0])
-
-    if diff > 0:
-        longest = lightChain
-        shortest = heavyChain
-        paddedShortest = []
-        for x in shortest:
-            paddedArray = np.pad(x, (0, diff), 'constant', constant_values=(0, np.inf))
-            paddedShortest.append(paddedArray)
-
-        paddedShortestToNumpyArray = np.array(paddedShortest)
-        return (lightChain, paddedShortestToNumpyArray)
-
-    elif diff < 0:
-        longest= heavyChain
-        shortest= lightChain
-        diff = abs(diff)
-
-        paddedShortest = []
-        for x in shortest:
-            paddedArray = np.pad(x, (0, diff), 'constant', constant_values=(0, np.inf))
-            paddedShortest.append(paddedArray)
-
-        paddedShortestToNumpyArray = np.array(paddedShortest)
-        return (paddedShortestToNumpyArray, heavyChain)
-
-    else:
-        return (lightChain, heavyChain)
+            light_id = chain_IDs[0]
+            heavy_id = chain_IDs[1]
+            antigen_id = chain_IDs[2]
 
 
-##
-# This method pads the chains to the max size of the antigen
-#
-#
-# #
-def expandAntibodyChainToMaxLength(chains, antigenLength, chainLength):
+            light_chain_distance_to_antigen = np.zeros(shape=(len(collection[antigen_id]), len(collection[light_id])))
 
-    returnArray = chains.copy()
-    if chainLength > len(chains[0][0]):
-        for x in range(0,len(chains)):
-            paddedChain = []
-            chainDiff = chainLength-len(chains[x][0])
+            self.caluculate_atom_distance(collection, light_id, antigen_id, light_chain_distance_to_antigen)
 
-            if chainDiff > 0:
-                paddedArrayColumns = []
-                for y in chains[x]:
-                    paddedArray = np.pad(y, (0, chainDiff), 'constant', constant_values=(0, np.inf))
-                    paddedArrayColumns.append(paddedArray)
+            heavy_chain_distance_to_antigen = np.zeros(shape=(len(collection[antigen_id]), len(collection[heavy_id])))
+            self.caluculate_atom_distance(collection, heavy_id, antigen_id, heavy_chain_distance_to_antigen)
+
+            padded_arrays = self.pad_chains_to_same_length(light_chain_distance_to_antigen, heavy_chain_distance_to_antigen)
+
+            structures.append([padded_arrays[0],padded_arrays[1]])
 
 
-                paddedChain = np.array(paddedArrayColumns)
-            returnArray[x] = paddedChain
+        padded_structures = self.find_longest_antigen_then_pad_to_that_length(structures)
 
-    if antigenLength > len(chains[0]):
-        for i in range(0,len(chains)):
-            antigenDiff = antigenLength - len(chains[i])
+        numpy_array = np.array(padded_structures)
 
-            if antigenDiff > 0:
-                for j in range(0,antigenDiff):
-                    emptyArr = np.full((1, chainLength), np.inf)
-                    returnArray[i] = numpy.append(returnArray[i], emptyArr, 0)
-
-    return returnArray
+        return numpy_array
 
 
-def isInCDR3(num):
-    if 117 >= num >= 105:
-        return True
-    else:
-        return False
+    def caluculate_atom_distance(self, collection, chain, antigen, list):
+        for x in range(len(collection[antigen])):
+            for i in range(len(collection[chain])):
+                a = np.array((collection[chain][i][0], collection[chain][i][1], collection[chain][i][2]))
+                b = np.array((collection[antigen][x][0], collection[antigen][x][1], collection[antigen][x][2]))
+
+                rounded = round(np.linalg.norm(a - b), 2)
+                list[x][i] = rounded
 
 
-def isInCDR2(num):
-    if 65 >= num >= 56:
-        return True
-    else:
-        return False
+
+    def find_longest_antigen_then_pad_to_that_length(self, structures):
+        longest_antigen = 0
+        longest_chain = 0
+
+        for x in structures:
+            if len(x[0]) > longest_antigen:
+                longest_antigen = len(x[0])
+            if len(x[0][0]) > longest_chain:
+                longest_chain = len(x[0][0])
+
+        padded_structures = []
+        for x in structures:
+            padded_structures.append(self.pad_antigens_to_same_length(x, longest_antigen, longest_chain))
+
+        return  padded_structures
 
 
-def isInCDR1(num):
-    if 38 >= num >= 27:
-        return True
-    else:
-        return False
+    def pad_chains_to_same_length(self, light_chain, heavy_chain):
+        diff = len(light_chain[0]) - len(heavy_chain[0])
+
+        if diff > 0:
+            longest = light_chain
+            shortest = heavy_chain
+            padded_shortest = []
+            for x in shortest:
+                padded_array = np.pad(x, (0, diff), 'constant', constant_values=(0, np.inf))
+                padded_shortest.append(padded_array)
+
+            padded_shortest_to_numpy_array = np.array(padded_shortest)
+            return (light_chain, padded_shortest_to_numpy_array)
+
+        elif diff < 0:
+            longest= heavy_chain
+            shortest= light_chain
+            diff = abs(diff)
+
+            padded_shortest = []
+            for x in shortest:
+                padded_array = np.pad(x, (0, diff), 'constant', constant_values=(0, np.inf))
+                padded_shortest.append(padded_array)
+
+            padded_shortest_to_numpy_array = np.array(padded_shortest)
+            return (padded_shortest_to_numpy_array, heavy_chain)
+
+        else:
+            return (light_chain, heavy_chain)
+
+
+
+    def pad_antigens_to_same_length(self, chains, antigen_length, chain_length):
+
+        return_array = chains.copy()
+        if chain_length > len(chains[0][0]):
+            for x in range(0, len(chains)):
+                padded_chain = []
+                chain_diff = chain_length - len(chains[x][0])
+
+                if chain_diff > 0:
+                    padded_array_columns = []
+                    for y in chains[x]:
+                        padded_array = np.pad(y, (0, chain_diff), 'constant', constant_values=(0, np.inf))
+                        padded_array_columns.append(padded_array)
+
+                    padded_chain = np.array(padded_array_columns)
+                return_array[x] = padded_chain
+
+        if antigen_length > len(chains[0]):
+            for i in range(0, len(chains)):
+                antigen_diff = antigen_length - len(chains[i])
+
+                if antigen_diff > 0:
+                    for j in range(0, antigen_diff):
+                        empty_arr = np.full((1, chain_length), np.inf)
+                        return_array[i] = numpy.append(return_array[i], empty_arr, 0)
+
+        return return_array
+
+    def get_carbon_alphas(self, parser_object):
+        collection = []
+        list = []
+
+        region = self.region_type
+        counter = 0
+
+        for model in parser_object:
+            for chain in model:
+                residue_counter = 0
+                for residue in chain:
+                    if residue_counter == 0:
+                        list.append(residue.get_parent())
+                        residue_counter = 1
+
+                    for atom in residue:
+                        if (atom.get_name() == "CA"):
+
+                            if "FULL_SEQUENCE" in region:
+                                list.append(atom.get_coord())
+
+                            else:
+                                if "IMGT_CDR3" in region and (
+                                        self.isInCDR3(atom.full_id[3][1]) or len(collection) >= 2):
+                                    list.append(atom.get_coord())
+
+                                if "IMGT_CDR2" in region and (
+                                        self.isInCDR2(atom.full_id[3][1]) or len(collection) >= 2):
+                                    list.append(atom.get_coord())
+
+                                if "IMGT_CDR1" in region and (
+                                        self.isInCDR1(atom.full_id[3][1]) or len(collection) >= 2):
+                                    list.append(atom.get_coord())
+
+                save_list = list.copy()
+                collection.append(save_list)
+                counter = counter + 1
+                list.clear()
+
+        return collection
+
+
+    def get_chain_ID(self, collection):
+        light_id = -1
+        heavy_id = -1
+        antigen_id = -1
+
+        for i in range(0, len(collection)):
+            chain_id = collection[i][0].id
+
+            if 'L' in chain_id or 'A' in chain_id:
+                light_id = i
+
+            elif 'H' in chain_id or 'B' in chain_id:
+                heavy_id = i
+
+            elif 'P' in chain_id or 'C' in chain_id:
+                antigen_id = i
+
+            collection[i].pop(0)
+
+        if light_id >= 0 and heavy_id >= 0 and antigen_id == -1:
+            if light_id + heavy_id == 1:
+                antigen_id = 2
+
+            elif light_id + heavy_id == 3:
+                antigen_id = 0
+
+        elif light_id == -1 or heavy_id == -1 or antigen_id == -1:
+            light_id = 0
+            heavy_id = 1
+            antigen_id = 2
+
+        return light_id, heavy_id, antigen_id
+
+
+    def isInCDR3(self, num):
+        if 117 >= num >= 105:
+            return True
+        else:
+            return False
+
+
+    def isInCDR2(self, num):
+        if 65 >= num >= 56:
+            return True
+        else:
+            return False
+
+
+    def isInCDR1(self, num):
+        if 38 >= num >= 27:
+            return True
+        else:
+            return False
+
