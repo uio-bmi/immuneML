44abe9bd669fe4676f54da7156c9af53bb3ede54
pavlovicmilena <milenapavlovic775@gmail.com>
2023-03-02 17:04:07 +0100

    update structure for Motif -> SeedMotif and PWM

--- immuneML/simulation/implants/PWM.py
+++ immuneML/simulation/implants/PWM.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 
-from bionumpy.io.motifs import read_motif
+from bionumpy.io.motifs import read_motif, Motif
 from bionumpy.sequence.position_weight_matrix import PWM as bnp_PWM
 
 from immuneML.environment.EnvironmentSettings import EnvironmentSettings
 from immuneML.environment.SequenceType import SequenceType
 
 
-class PWM:
+class PWM(Motif):
     """
     Class describing positional weight matrix and using bionumpy's PWM internally.
 
     Arguments:
+
         file_path: path to the file where the PWM is stored
 
         threshold (float): when matching PWM to a sequence, this is the threshold to consider the sequence as containing the motif
@@ -28,20 +29,20 @@ class PWM:
 
     """
 
-    def __init__(self, file_path: Path, matrix: bnp_PWM, threshold: float):
+    def __init__(self, file_path: Path, pwm_matrix: bnp_PWM, threshold: float):
 
         self.file_path = file_path
-        self.matrix = matrix
+        self.pwm_matrix = pwm_matrix
         self.threshold = threshold
 
-        assert self.matrix is not None or self.file_path is not None, (file_path, matrix)
+        assert self.pwm_matrix is not None or self.file_path is not None, (file_path, pwm_matrix)
 
     @classmethod
     def build(cls, file_path, threshold: float):
         assert Path(file_path).is_file(), file_path
-        matrix = read_motif(file_path)
-        return PWM(file_path, matrix, threshold)
+        pwm_matrix = read_motif(file_path)
+        return PWM(file_path, pwm_matrix, threshold)
 
     def get_all_motif_instances(self, sequence_type: SequenceType):
-        assert sorted(self.matrix.alphabet) == sorted(EnvironmentSettings.get_sequence_alphabet(sequence_type))
+        assert sorted(self.pwm_matrix.alphabet) == sorted(EnvironmentSettings.get_sequence_alphabet(sequence_type))
         return self
