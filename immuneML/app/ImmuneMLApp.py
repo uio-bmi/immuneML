4cc73d8eefdffaaff5f8d898b9bf03ba91fe072d
jorgeski <jorgeski@student.matnat.uio.no>
2023-03-13 16:21:14 +0100

    Added code that generates new name for output dir if it already exists. Meant for simplifying testing

--- immuneML/app/ImmuneMLApp.py
+++ immuneML/app/ImmuneMLApp.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import warnings
 from pathlib import Path
+import random
 
 from immuneML.caching.CacheType import CacheType
 from immuneML.dsl.ImmuneMLParser import ImmuneMLParser
@@ -59,7 +60,13 @@ class ImmuneMLApp:
 
 def run_immuneML(namespace: argparse.Namespace):
     if os.path.isdir(namespace.result_path) and len(os.listdir(namespace.result_path)) != 0:
-        raise ValueError(f"Directory {namespace.result_path} already exists. Please specify a new output directory for the analysis.")
+        # raise ValueError(f"Directory {namespace.result_path} already exists. Please specify a new output directory for the analysis.")
+        # --------------
+        # TODO: remove this temporary code that generates new name for output. Only meant to simplify testing
+        print_log(f"Directory {namespace.result_path} already exists. generating id", include_datetime=True)
+        namespace.result_path = Path(str(namespace.result_path) + "_" + str(random.randint(0, 100000)))
+        print_log(namespace.result_path)
+        # --------------
     PathBuilder.build(namespace.result_path)
 
     logging.basicConfig(filename=Path(namespace.result_path) / "log.txt", level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
