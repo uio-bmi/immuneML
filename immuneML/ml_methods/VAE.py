37ffc2cbca947c3b8378640f0edf3b4d35d4e74b
Theodor Bergersen <mtberger@uio.no>
2023-05-02 11:50:59 +0200

    Revert "VAE changes, not working version"
    This reverts commit fbacd3485aa6f1d5270cf1cc327c46913b8203e9.

--- immuneML/ml_methods/VAE.py
+++ immuneML/ml_methods/VAE.py
@@ -1,6 +1,4 @@
-import random
 
-import keras
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -11,7 +9,6 @@ from immuneML.ml_methods.GenerativeModel import GenerativeModel
 from scripts.specification_util import update_docs_per_mapping
 from immuneML.util.PathBuilder import PathBuilder
 from tensorflow.python.framework.ops import disable_eager_execution
-from immuneML.ml_methods.VAE_model import VAE_model
 
 
 class VAE(GenerativeModel):
@@ -23,92 +20,25 @@ class VAE(GenerativeModel):
         parameters = parameters if parameters is not None else {}
         parameter_grid = parameter_grid if parameter_grid is not None else {}
         super(VAE, self).__init__(parameter_grid=parameter_grid, parameters=parameters)
-        # disable_eager_execution()
-
-        self.latent_dim = self._parameters["latent_dim"]
-        self.layers = self._parameters["layers"]
-        self.epochs = self._parameters["epochs"]
-        self.batch_size = self._parameters["batch_size"]
-
+        disable_eager_execution()
         self.encoder = None
         self.decoder = None
         self.generated_sequences = []
 
-
-
-    def sampler(self, latent_dim, epsilon_std=1):
-        _sampling = lambda z_args: (z_args[0] + K.sqrt(z_args[1] + 1e-8) *
-                                    K.random_normal(shape=K.shape(z_args[0]), mean=0., stddev=epsilon_std))
-
-        return tf.keras.layers.Lambda(_sampling, output_shape=(latent_dim,))
-
-    def _get_encoder(self, seq_length, vocab_size, latent_space, layers=4, num_filters=21,
-                     max_filters=10000, kernel_size=3):
-
-        x = tf.keras.layers.Input((seq_length, vocab_size), name="encoder_input")
-        h = x
-        for i in range(layers):
-            h = tf.keras.layers.Conv1D(min(num_filters*(2**i), max_filters), kernel_size,
-                                       strides=1)(h)
-            h = tf.keras.layers.BatchNormalization(momentum=0.9)(h)
-            h = tf.keras.layers.Dense(h.shape[-1], activation='PReLU')(h)
-
-        h = tf.keras.layers.Flatten()(h)
-
-        z_mean = tf.keras.layers.Dense(latent_space)(h)
-        z_var = tf.keras.layers.Dense(latent_space, activation='softplus')(h)
-        z = self.sampler(latent_space)([z_mean, z_var])
-
-        encoder = tf.keras.models.Model(x, [z_mean, z_var, z])
-
-        return encoder
-
-    def _get_decoder(self, seq_length, vocab_size, latent_space=2, ncell=512, project_x=True,
-                               upsample=True, min_deconv_dim=42,
-                               input_dropout=0.45, intermediate_dim=63,
-                               max_filters=336, layers=3):
-        latent_vector = tf.keras.layers.Input((latent_space,))
-
-        prot_oh = tf.keras.layers.Input((seq_length, vocab_size))
-        input_x = tf.keras.layers.ZeroPadding1D(padding=(1,0))(prot_oh)
-        input_x = tf.keras.layers.Lambda(lambda x_: x_[:,:-1,:])(input_x)
-
-        if input_dropout is not None:
-            input_x = tf.keras.layers.Dropout(input_dropout, noise_shape=(None, seq_length, 1))(input_x)
-        if project_x:
-            input_x = tf.keras.layers.Conv1D(vocab_size, 1, activation=None, name='decoder_x_embed')(input_x)
-
-        rnn = tf.keras.layers.GRU(ncell, return_sequences=True)
-        if upsample:
-            z_seq = self.upsampler(latent_vector, intermediate_dim, min_deconv_dim=min_deconv_dim,
-                              layers=layers, max_filters=max_filters)
-        else:
-            z_seq = tf.keras.layers.RepeatVector(seq_length)(latent_vector)
-
-        z_seq = tf.keras.layers.Flatten()(z_seq)
-        z_seq = tf.keras.layers.Dense(seq_length*vocab_size)(z_seq)
-        z_seq = tf.keras.layers.Reshape((seq_length, vocab_size))(z_seq)
-        xz_seq = tf.keras.layers.Concatenate(axis=-1)([z_seq, input_x])
-        rnn_out = rnn(xz_seq)
-
-        processed_x = tf.keras.layers.Conv1D(vocab_size, 1, activation=None, use_bias=True)(rnn_out)
-        output = tf.keras.layers.Activation('softmax')(processed_x)
-
-        decoder = tf.keras.models.Model([latent_vector, prot_oh], output)
-        return decoder
-
     def upsampler(self, latent_vector, low_res_dim, min_deconv_dim=21,
-                  layers=4, kernel_size=3, max_filters=336, aim=28):
+                  n_deconv=3, kernel_size=2, BN=True, dropout=None,
+                  activation='relu', max_filters=336, aim=28):
 
-        low_res_features = min(min_deconv_dim * (2 ** layers), max_filters)
+        low_res_features = min(min_deconv_dim * (2 ** n_deconv), max_filters)
         h = tf.keras.layers.Dense(low_res_dim * low_res_features, name='upsampler_mlp')(latent_vector)
         h = tf.keras.layers.Reshape((low_res_dim, low_res_features))(h)
 
-        for i in range(layers):
+        for i in range(n_deconv):
             shape = list(h.shape[1:])
+            assert len(shape) == 2, "The input should have a width and a depth dimensions (plus the batch dimensions)"
             new_shape = shape[:-1] + [1] + [shape[-1]]
             h = tf.keras.layers.Reshape(new_shape)(h)
-            filters = min(min_deconv_dim * 2 ** (layers - (i + 1)), max_filters)
+            filters = min(min_deconv_dim * 2 ** (n_deconv - (i + 1)), max_filters)
             h = tf.keras.layers.Conv2DTranspose(filters,
                                 kernel_size,
                                 strides=(2, 1),
@@ -124,21 +54,72 @@ class VAE(GenerativeModel):
             h = tf.keras.layers.Reshape(new_shape)(h)
 
             h = tf.keras.layers.Dense(h.shape[2], activation='PReLU')(h)
-
+        h = tf.keras.layers.Flatten()(h)
+        h = tf.keras.layers.Dense(aim * new_shape[1])(h)
+        h = tf.keras.layers.Reshape((aim, new_shape[1]))(h)
         return h
 
+    def _get_encoder(self, seq_length, vocab_size, latent_space=2, initial_units=256, layers=4, num_filters=21,
+                     max_filters=10000, kernel_size=2, activation='PReLU'):
 
-    def _get_ml_model(self, latent_dim, one_hot):
+        x = tf.keras.layers.Input((seq_length, vocab_size), name="encoder_input")
+        for i in range(layers):
+            h = tf.keras.layers.Conv1D(min(num_filters*(2**i), max_filters), kernel_size,
+                                       strides=1 if i==0 else 2)(x)
+            h = tf.keras.layers.BatchNormalization(momentum=0.9)(h)
+            h = tf.keras.layers.Dense(h.shape[2], activation='PReLU')(h)
 
-        self.encoder = self._get_encoder(one_hot.shape[1], one_hot.shape[2], latent_space=self.latent_dim, layers=self.layers)
-        self.decoder = self._get_decoder(one_hot.shape[1], one_hot.shape[2], latent_dim, layers=self.layers)
+        h = tf.keras.layers.Flatten()(h)
+        z_mean = tf.keras.layers.Dense(latent_space)(h)
+        z_var = tf.keras.layers.Dense(latent_space, activation='softplus')(h)
 
-        self.model = VAE_model(self.encoder, self.decoder)
+        encoder = tf.keras.models.Model(x, [z_mean, z_var])
 
-        self.model.compile(loss='mse', optimizer='adam')
+        return encoder
 
+    def _get_decoder(self, seq_length, vocab_size, latent_space=2, ncell=512, project_x=True,
+                               upsample=True, min_deconv_dim=42,
+                               input_dropout=0.45, intermediate_dim=63,
+                               max_filters=336, n_conditions=0,
+                               cond_concat_each_timestep=False):
+        latent_vector = tf.keras.layers.Input((latent_space,))
+
+        prot_oh = tf.keras.layers.Input((seq_length, vocab_size))
+        input_x = tf.keras.layers.ZeroPadding1D(padding=(1,0))(prot_oh)
+        input_x = tf.keras.layers.Lambda(lambda x_: x_[:,:-1,:])(input_x)
+
+        if input_dropout is not None:
+            input_x = tf.keras.layers.Dropout(input_dropout, noise_shape=(None, seq_length, 1))(input_x)
+        if project_x:
+            input_x = tf.keras.layers.Conv1D(vocab_size, 1, activation=None, name='decoder_x_embed')(input_x)
+
+        rnn = tf.keras.layers.GRU(ncell, return_sequences=True)
+        if upsample:
+            z_seq = self.upsampler(latent_vector, intermediate_dim, min_deconv_dim=min_deconv_dim,
+                              n_deconv=3, activation='prelu', max_filters=max_filters)
+        else:
+            z_seq = tf.keras.layers.RepeatVector(seq_length)(latent_vector)
+
+        xz_seq = tf.keras.layers.Concatenate(axis=-1)([z_seq, input_x])
+        rnn_out = rnn(xz_seq)
+
+        processed_x = tf.keras.layers.Conv1D(vocab_size, 1, activation=None, use_bias=True)(rnn_out)
+        output = tf.keras.layers.Activation('softmax')(processed_x)
+
+        decoder = tf.keras.models.Model([latent_vector, prot_oh], output)
+        return decoder
+
+    def _get_ml_model(self):
+        pass
+
+    def sampler(self, latent_dim, epsilon_std=1):
+        _sampling = lambda z_args: (z_args[0] + K.sqrt(tf.convert_to_tensor(z_args[1] + 1e-8, np.float32)) *
+                                    K.random_normal(shape=K.shape(z_args[0]), mean=0., stddev=epsilon_std))
+
+        return tf.keras.layers.Lambda(_sampling, output_shape=(latent_dim,))
 
     def _fit(self, dataset, cores_for_training: int = 1, result_path: Path = None):
+        latent_dim = 2
 
         dataset = np.insert(dataset, 0, 0, axis=2)
 
@@ -158,44 +139,74 @@ class VAE(GenerativeModel):
 
         one_hot = np.array(new_data)
 
-        self._get_ml_model(self.latent_dim, one_hot)
+        sampler = self.sampler(latent_dim)
+        self.encoder = self._get_encoder(one_hot.shape[1], one_hot.shape[2])
+        self.decoder = self._get_decoder(one_hot.shape[1], one_hot.shape[2])
+        prot = self.encoder.inputs[0]
+        encoder_inp = [prot]
+        vae_inp = [prot]
+
+        z_mean, z_var = self.encoder(encoder_inp)
+        z = sampler([z_mean, z_var])
+
+        decoder_inp = [z, prot]
+
+        decoded = self.decoder(decoder_inp)
+
+        self.model = tf.keras.models.Model(inputs=vae_inp, outputs=decoded)
+
+        def xent_loss(x, x_d_m):
+            return K.sum(tf.keras.losses.categorical_crossentropy(x, x_d_m), -1)
+
+        def kl_loss(x, x_d_m):
+            return - 0.5 * K.sum(1 + K.log(z_var + 1e-8) - K.square(z_mean) - z_var, axis=-1)
+
+        def vae_loss(x, x_d_m):
+            return xent_loss(x, x_d_m) + kl_loss(x, x_d_m)
+
+        self.model.compile(loss=vae_loss, optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.001))
+        print('Protein VAE initialized !')
+
+        #self._get_ml_model(one_hot.shape[1], one_hot.shape[2], initial_units=32)
 
         train_len = int(one_hot.shape[0] / 2)
-        x_train = one_hot[:train_len - (train_len % self.batch_size)]
+        x_train = one_hot[:train_len]
         x_test = one_hot[train_len:train_len + int((train_len / 2))]
         x_val = one_hot[train_len + int((train_len / 2)):]
 
         self.encoder.summary()
         self.decoder.summary()
-        # self.model.summary()
+        self.model.summary()
 
         self.model.fit(x_train,
                        x_train,
-                       epochs=self.epochs,
-                       # verbose=0,
-                       batch_size=self.batch_size,
+                       epochs=1,
+                       batch_size=32,
                        shuffle=True,
                        validation_data=(x_test,
                                         x_test),
                        workers=cores_for_training)
 
+
         return self.model
 
     def generate(self, amount=10, mean=0, stddev=1, path_to_model: Path = None):
 
-        z = mean + stddev * np.random.randn(amount, self.latent_dim)
-        # x, y = z.transpose()
-        # plt.scatter(x, y)
-        # plt.show()
-        print("Generating " + str(amount) + " sequences")
+        z = mean + stddev * np.random.randn(amount, 2)
+        x, y = z.transpose()
+        plt.scatter(x, y)
+        plt.show()
+
         original_dim, alphabet_size = self.decoder.output_shape[1], self.decoder.output_shape[-1]
         x = np.zeros((z.shape[0], original_dim, alphabet_size))
+        start = 0
+        seqs = ""
 
-        for i in range(0, original_dim):
+        for i in range(start, original_dim):
             # iteration is over positions in sequence, which can't be parallelized
-            pred = self.decoder(z)
+            pred = self.decoder.predict([z, x])
             pos_pred = pred[:, i, :]
-            pred_ind = tf.argmax(pos_pred, -1)  # convert probability to index
+            pred_ind = pos_pred.argmax(-1)  # convert probability to index
             for j, p in enumerate(pred_ind):
                 x[j, i, p] = 1
 
