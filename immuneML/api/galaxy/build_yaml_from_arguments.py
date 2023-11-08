6c2986cd75989fecbb8f3a2fc3706bf6bef6548a
Lonneke Scheffer <lonsch96@live.nl>
2023-11-08 11:15:01 +0100

    refactoring

--- immuneML/api/galaxy/build_yaml_from_arguments.py
+++ immuneML/api/galaxy/build_yaml_from_arguments.py
@@ -131,10 +131,10 @@ def build_settings_specs(enc_names, ml_names):
 
 
 def discover_dataset_params():
-    dataset = glob.glob("*.yaml")
+    dataset = glob.glob("*dataset.yaml")
 
-    assert len(dataset) > 0, "no .yaml file was present in the current working directory"
-    assert len(dataset) < 2, "multiple .yaml files were present in the current working directory"
+    assert len(dataset) > 0, "no *dataset.yaml file was present in the current working directory"
+    assert len(dataset) < 2, f"multiple *dataset.yaml files were present in the current working directory: {list(glob.glob('*dataset.yaml'))}"
 
     dataset_path = dataset[0]
 
