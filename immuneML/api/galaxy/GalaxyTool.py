c0ddd5b5e95ef56cc1b4fbadbaff439d10042727
Lonneke Scheffer <lonsch96@live.nl>
2024-10-12 11:06:43 -0700

    test better error handling in galaxy

--- immuneML/api/galaxy/GalaxyTool.py
+++ immuneML/api/galaxy/GalaxyTool.py
@@ -12,10 +12,19 @@ class GalaxyTool(metaclass=ABCMeta):
         self.result_path = Path(os.path.relpath(result_path)) if result_path is not None else None
 
     def run(self):
-        self._run()
+        try:
+            self._run()
+        except Exception as e:
+            self._make_failed_galaxy_run_html()
+            raise e
+
         shutil.make_archive(Path("./immuneML_output"), "zip", self.result_path)
         shutil.move(str(Path("./immuneML_output.zip")), str(self.result_path))
 
     @abc.abstractmethod
     def _run(self):
+        pass
+
+    def _make_failed_galaxy_run_html(self):
+        print("test")
         pass
