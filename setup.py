bddd8d9de91f4da86d95299b2cfa02805a8636d4
pavlovicmilena <milenapavlovic775@gmail.com>
2024-11-01 11:11:14 +0100

    fix installation stuff

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ setup(
     author="immuneML Team",
     author_email="milenpa@student.matnat.uio.no",
     url="https://github.com/uio-bmi/immuneML",
-    install_requires=["pandas>=1,<2.0", "PyYAML>=5.3", "scikit-learn>=0.23",
+    install_requires=["numpy", "pandas>=1,<2.0", "PyYAML>=5.3", "scikit-learn>=0.23",
                       "matplotlib>=3.1", "editdistance", "regex", "tzlocal", "airr>=1,<1.4",
                       "pystache", "dill>=0.3", "plotly>=4", "matplotlib-venn>=0.11", "scipy", "bionumpy>=0.2.31",
-                      "umap-learn", "olga", "numpy"],
+                      "umap-learn", "olga"],
     extras_require={
         'word2vec': ['gensim'],
         'fisher': ['fisher', 'fishersapi'],
