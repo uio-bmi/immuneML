ffe9619e1b839257881708c849277c7617002020
pavlovicmilena <milenapavlovic775@gmail.com>
2024-11-04 11:56:39 +0100

    limit scipy version because it doesn't work with gensim

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url="https://github.com/uio-bmi/immuneML",
     install_requires=["numpy<2.0.0", "pandas", "PyYAML>=5.3", "scikit-learn>=0.23",
                       "matplotlib>=3.1", "editdistance", "regex", "tzlocal", "airr>=1,<1.4",
-                      "pystache", "dill>=0.3", "plotly>=4", "matplotlib-venn>=0.11", "scipy", "bionumpy>=0.2.31",
+                      "pystache", "dill>=0.3", "plotly>=4", "matplotlib-venn>=0.11", "scipy<1.13", "bionumpy>=0.2.31",
                       "umap-learn", "olga"],
     extras_require={
         'word2vec': ['gensim'],
