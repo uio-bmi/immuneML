8bf165bedbffda6bcd475a3dcd4b0fa5af5a9235
Lonneke Scheffer <lonsch96@live.nl>
2024-11-06 16:41:18 -0800

    minor: improved display SequenceCountDistribution (x axis sorted)

--- immuneML/reports/data_reports/SequenceCountDistribution.py
+++ immuneML/reports/data_reports/SequenceCountDistribution.py
@@ -138,7 +138,6 @@ class SequenceCountDistribution(DataReport):
                         labels={"n_observations": "Number of observations",
                                 "duplicate_count": "Sequence duplicate count"})
         figure.update_layout(template="plotly_white")
-        figure.update_xaxes(row=1, type="category")
         PathBuilder.build(self.result_path)
 
         file_path = self.result_path / "sequence_count_distribution.html"
