0932375b7320d573721bdbd84ea7a1466b5d3b56
Lonneke Scheffer <lonsch96@live.nl>
2024-12-19 16:32:55 -0800

    bugfix dataset.yaml by PWM

--- immuneML/ml_methods/generative_models/PWM.py
+++ immuneML/ml_methods/generative_models/PWM.py
@@ -147,16 +147,17 @@ class PWM(GenerativeModel):
                            'gen_model_name': [self.name for _ in range(count)]})
 
         df = make_full_airr_seq_set_df(df)
+        filename = str(PathBuilder.build(path) / 'synthetic_dataset.tsv')
 
-        df.to_csv(str(PathBuilder.build(path) / 'synthetic_dataset.tsv'), sep='\t', index=False)
+        df.to_csv(filename, sep='\t', index=False)
 
-        write_dataset_yaml(path / 'synthetic_metadata.yaml', {
-            'dataset_type': 'SequenceDataset',
-            'type_dict_dynamic_fields': {'gen_model_name': 'str'},
-            'name': 'synthetic_dataset', 'labels': {'gen_model_name': [self.name]},
-            'timestamp': str(datetime.now()),
-            "identifier": uuid4().hex
-        })
+        dataset_yaml = SequenceDataset.create_metadata_dict(SequenceDataset,
+                                             filename=filename,
+                                             type_dict={'gen_model_name': str},
+                                             name="synthetic_dataset",
+                                             labels={'gen_model_name': [self.name]})
+
+        write_dataset_yaml(path / 'synthetic_metadata.yaml', dataset_yaml)
 
         return SequenceDataset.build(path / 'synthetic_dataset.tsv', path / 'synthetic_metadata.yaml',
                                      'synthetic_dataset')
