2404b5d8ccb92b493e3e22d247a9675696c48df5
pavlovicmilena <milenapavlovic775@gmail.com>
2025-06-15 14:30:34 +0200

    update sig annotation test

--- test/simulation/test_signal_annotation.py
+++ test/simulation/test_signal_annotation.py
@@ -1,10 +1,11 @@
 import shutil
 
+import numpy as np
+
 from immuneML.data_model.SequenceParams import RegionType
 from immuneML.environment.EnvironmentSettings import EnvironmentSettings
 from immuneML.environment.SequenceType import SequenceType
 from immuneML.simulation.dataset_generation.RandomDatasetGenerator import RandomDatasetGenerator
-from immuneML.simulation.implants.Motif import Motif
 from immuneML.simulation.implants.SeedMotif import SeedMotif
 from immuneML.simulation.implants.Signal import Signal
 from immuneML.simulation.util.signal_annotation import annotate_sequence_dataset
@@ -12,7 +13,6 @@ from immuneML.util.PathBuilder import PathBuilder
 
 
 def test_annotate_sequence_dataset():
-
     path = PathBuilder.remove_old_and_build(EnvironmentSettings.tmp_test_path / "annotated_sequences/")
 
     dataset = RandomDatasetGenerator.generate_sequence_dataset(10, {2: 1.}, {'model': {'A': 0.5, 'B': 0.5}}, path)
@@ -21,6 +21,9 @@ def test_annotate_sequence_dataset():
     annotated_sequences = annotate_sequence_dataset(dataset, signals, region_type=RegionType.IMGT_CDR3,
                                                     sequence_type=SequenceType.AMINO_ACID)
 
-    print(annotated_sequences.head())
+    assert np.array_equal(annotated_sequences.columns, ['cdr3_aa', 'cdr3', 'v_call', 'j_call', 'locus', 's1', 's2',
+                                                        's1_positions', 's2_positions', 'signals_aggregated'])
+
+    assert annotated_sequences.shape == (10, 10)
 
     shutil.rmtree(path)
