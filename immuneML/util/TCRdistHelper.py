8b59f526d8bf0d81c3f1ddab85572df568d6841b
pavlovicmilena <milenapavlovic775@gmail.com>
2025-08-08 14:13:45 +0200

    fix: tcrdist to use proper label names

--- immuneML/util/TCRdistHelper.py
+++ immuneML/util/TCRdistHelper.py
@@ -69,8 +69,6 @@ class TCRdistHelper:
 
         df = dataset.data.topandas()
 
-        epitope_name = 'epitope' if 'epitope' in df.columns else 'Epitope' if 'Epitope' in df.columns else ''
-
         if "subject" not in df:
             df['subject'] = "sub" + df['cell_id']
 
@@ -81,7 +79,7 @@ class TCRdistHelper:
         unique_chains = [str(Chain.get_chain(el)).lower() for el in df['locus'].unique().tolist()]
 
         df['clone_id'] = df['cell_id' if len(unique_chains) == 2 else 'sequence_id']
-        cols_to_keep = ['cdr3', 'cdr3_aa', 'v_call', 'j_call', 'duplicate_count', 'subject', epitope_name, 'clone_id']
+        cols_to_keep = ['cdr3', 'cdr3_aa', 'v_call', 'j_call', 'duplicate_count', 'subject', 'clone_id'] + label_names
 
         df_alpha, df_beta = None, None
 
@@ -104,6 +102,6 @@ class TCRdistHelper:
                 df = df_beta
 
         if len(unique_chains) == 2:
-            df = df_alpha.merge(df_beta, on=['clone_id', epitope_name, 'subject', 'count'])
+            df = df_alpha.merge(df_beta, on=['clone_id', 'subject', 'count'] + label_names)
 
         return df, unique_chains
